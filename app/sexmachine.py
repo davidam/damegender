b688cb873236b9749620340b402586c9426f395d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-10-11 11:21:01 +0200

    sexmachine.py: creation

---
+++ app/sexmachine.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: David Arroyo Menéndez.
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import nltk
+from nltk.corpus import names
+from pprint import pprint
+
+class Sexmachine(object):
+    def features(self, name):
+        features = {}
+        features["first_letter"] = name[0].lower()
+        features["last_letter"] = name[-1].lower()
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            features["count({})".format(letter)] = name.lower().count(letter)
+            features["has({})".format(letter)] = (letter in name.lower())
+        return features
+
+    def guess(self, name):
+        labeled_names = ([(name, 'male') for name in names.words('male.txt')] +
+                         [(name, 'female') for name in names.words('female.txt')])
+        featuresets = [(self.features(n), gender) for (n, gender) in labeled_names]
+        train_set, test_set = featuresets[500:], featuresets[:500]
+        classifier = nltk.NaiveBayesClassifier.train(train_set)
+        return classifier.classify(self.features(name))
+
+s = Sexmachine()
+print(s)
+f = s.features("David")
+print(f['has(a)'])
+print(f['count(i)'])
+print(f['count(v)'])
+print(f['last_letter'])
+print(f['first_letter'])
+print(s.guess("David"))
