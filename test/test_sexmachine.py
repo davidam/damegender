568b06a37cce1fdf3317c72f58476d6e6eef7c4d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-10-20 09:03:30 +0200

    test/test_sexmachine.py: add multinomialNB

--- test/test_sexmachine.py
+++ test/test_sexmachine.py
@@ -79,7 +79,7 @@ class TddInPythonExample(unittest.TestCase):
         s = Sexmachine()
         gl = s.gender_list()
         self.assertEqual(gl, [1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1])
-#        self.assertEqual(len(gl), 21)
+        self.assertEqual(len(gl), 21)
 
     def test_sexmachine_gaussianNB_list_method_returns_correct_result(self):
         s = Sexmachine()
@@ -98,13 +98,13 @@ class TddInPythonExample(unittest.TestCase):
         predicted = m.predict([[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0, 0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0]])
         self.assertTrue(predicted, [[1]])
 
-    # def test_sexmachine_multinomialNB_list_method_returns_correct_result(self):
-    #     s = Sexmachine()
-    #     m = s.multinomialNB()
-    #     array = [[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0,
-    #                0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0],
-    #              [ 0,  0,  0,  0, 21,  0,  0,  0,  0, 34,  0,  0,  0,  0,  0,  1,
-    #                0,  0,  0,  5,  0,  0,  1,  0,  0,  1,  0,  0,  1, 34,  0,  0]]
-    #     predicted= m.predict(array)
-    #     n = np.array([2, 2])
-    #     self.assertTrue(np.array_equal(predicted, n))
+    def test_sexmachine_multinomialNB_list_method_returns_correct_result(self):
+        s = Sexmachine()
+        m = s.multinomialNB()
+        array = [[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0,
+                   0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0],
+                 [ 0,  0,  0,  0, 21,  0,  0,  0,  0, 34,  0,  0,  0,  0,  0,  1,
+                   0,  0,  0,  5,  0,  0,  1,  0,  0,  1,  0,  0,  1, 34,  0,  0]]
+        predicted= m.predict(array)
+        n = np.array([2, 2])
+        self.assertTrue(np.array_equal(predicted, n))
