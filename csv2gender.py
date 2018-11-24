b1b9bdf2907e2a6fb7815a873ded5f5e11e57025
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-11-24 04:26:54 +0100

    csv2gender.py: now running with partial.csv and all.csv

--- csv2gender.py
+++ csv2gender.py
@@ -30,8 +30,13 @@ parser.add_argument('--version', action='version', version='0.1')
 args = parser.parse_args()
 if (len(sys.argv) > 1):
     s = Sexmachine()
-    s.gender_list()
+    if (args.file == 'files/partial.csv'):
+        s.gender_list()
+    elif (args.file == 'files/all.csv'):
+        s.gender_list(all=True)
+    else:
+        print("Perhaps this file has not the right format")
     print("The number of males in %s is %s" % (str(args.file), str(s.males)))
-    print("The number of females in %s is %s" % (str(args.file), str(s.males)))
+    print("The number of females in %s is %s" % (str(args.file), str(s.females)))
     print("The number of gender not recognised in %s is %s" % (str(args.file), str(s.unknown)))
 
