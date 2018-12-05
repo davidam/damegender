411220ba0cfe9d2078d127e95ae8e316dd0e9d64
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-05 18:41:54 +0100

    app/gendernamsor.py: guess only with the api

--- app/gendernamsor.py
+++ app/gendernamsor.py
@@ -32,14 +32,14 @@ class Gendernamsor(Gender):
     def guess(self, name, surname, binary=False):
     # guess method to check names dictionary
         namsorlist = []
-        guess = super().guess(name, binary)
-        if ((guess == 'unknown') | (guess == 2)):
-            r = requests.get('https://api.namsor.com/onomastics/api/json/gender/'+ name +'/' + surname)
-            d = json.loads(r.text)
-            if ((d['gender'] == 'female') and binary):
-                guess = 0
-            elif ((d['gender'] == 'male') and binary):
-                guess = 1
-            elif (not(binary)):
-                guess = d['gender']
+        # guess = super().guess(name, binary)
+        # if ((guess == 'unknown') | (guess == 2)):
+        r = requests.get('https://api.namsor.com/onomastics/api/json/gender/'+ name +'/' + surname)
+        d = json.loads(r.text)
+        if ((d['gender'] == 'female') and binary):
+            guess = 0
+        elif ((d['gender'] == 'male') and binary):
+            guess = 1
+        elif (not(binary)):
+            guess = d['gender']
         return guess
