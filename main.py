57d25e569e7fb8dc96a3e6653b95da8dd57208ba
David Arroyo Menendez <davidam@gmail.com>
2018-12-10 00:06:54 +0100

    main.py: sexmachine to dame_sexmachine

--- main.py
+++ main.py
@@ -21,7 +21,7 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-from app.sexmachine import Sexmachine
+from app.dame_sexmachine import Sexmachine
 import sys
 import argparse
 parser = argparse.ArgumentParser()
