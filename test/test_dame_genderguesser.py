dc19e7e50eb15ca43c9963ccf9abc68a760fc91a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-12 18:22:08 +0100

    test/test_dame_genderguesser.py: Genderguesser to DameGenderGuesser and add accuracy method

--- test/test_dame_genderguesser.py
+++ test/test_dame_genderguesser.py
@@ -1,11 +1,15 @@
 import unittest
-from app.dame_genderguesser import Genderguesser
+from app.dame_genderguesser import DameGenderGuesser
 
 class TddInPythonExample(unittest.TestCase):
 
     def test_genderguesser_list_method_returns_correct_result(self):
-        dgg = Genderguesser()
+        dgg = DameGenderGuesser()
         self.assertEqual(dgg.guess("Sara", binary=False), "female")
         self.assertEqual(dgg.guess("Sara", binary=True), 0)
         self.assertEqual(dgg.guess("Laura", binary=False), "female")
         self.assertEqual(dgg.guess("Laura", binary=True), 0)
+
+    def test_dame_genderguesser_accuracy_method_returns_correct_result(self):
+        dgg = DameGenderGuesser()
+        self.assertTrue(dgg.accuracy(path="files/partial.csv") >= 0.5)
