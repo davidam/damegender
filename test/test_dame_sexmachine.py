5d43626387b1666e456b54d53bce227c979792d9
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-13 16:30:01 +0100

    test/test_dame_sexmachine.py: Sexmachine to DameSexmachine

--- test/test_dame_sexmachine.py
+++ test/test_dame_sexmachine.py
@@ -23,19 +23,19 @@
 
 import unittest
 import numpy as np
-from app.dame_sexmachine import Sexmachine
+from app.dame_sexmachine import DameSexmachine
 
 class TddInPythonExample(unittest.TestCase):
 
     def test_string2array_method_returns_correct_result(self):
         array = "muchos    espacios en blanco"
-        s = Sexmachine()
+        s = DameSexmachine()
         arr = s.string2array(array)
         self.assertEqual(["muchos", "espacios", "en", "blanco"], arr)
 
 
     def test_sexmachine_features_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         f = s.features("David")
         self.assertEqual(f['has(a)'], True)
         self.assertEqual(f['count(i)'], 1)
@@ -44,13 +44,13 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(f['first_letter'], 'd')
 
     def test_sexmachine_features_int_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         f = s.features_int("David")
         self.assertTrue(f['syllables'] == 2)
         self.assertTrue(len(f) > 0)
 
     def test_sexmachine_guess_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         self.assertEqual(s.guess("David"), 'male')
         self.assertEqual(s.guess("Laura"), 'female')
         self.assertEqual(s.guess("Inés"), 'female') # Con acento
@@ -63,17 +63,17 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(s.guess("Nadiccionaria", binary=True), 0)
 
     def test_sexmachine_guess_surname_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         self.assertTrue(s.guess_surname("Smith"))
 
     def test_sexmachine_guess_list_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         self.assertEqual(['male', 'male', 'female', 'male', 'male', 'male', 'female', 'female', 'male', 'female', 'male', 'male', 'male', 'male', 'male', 'female', 'male', 'male', 'male', 'male', 'male'], s.guess_list(all=False, binary=False))
         self.assertEqual([1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1], s.guess_list(all=False,binary=True))
 #        self.assertEqual([('"pierre"', 'male'), ('"raul"', 'male'), ('"adriano"', 'female'), ('"ralf"', 'male'), ('"teppei"', 'male'), ('"guillermo"', 'male'), ('"catherine"', 'female'), ('"sabina"', 'female'), ('"ralf"', 'male'), ('"karl"', 'female'), ('"sushil"', 'male'), ('"clemens"', 'male'), ('"gregory"', 'male'), ('"lester"', 'male'), ('"claude"', 'male'), ('"martin"', 'female'), ('"vlad"', 'male'), ('"pasquale"', 'male'), ('"lourdes"', 'male'), ('"bruno"', 'male'), ('"thomas"', 'male')], s.guess_list())
 
     def test_sexmachine_features_int_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         dicc = s.features_int("David")
         self.assertEqual(chr(dicc['last_letter']), 'd')
         self.assertEqual(chr(dicc['first_letter']), 'd')
@@ -94,17 +94,17 @@ class TddInPythonExample(unittest.TestCase):
         self.assertTrue(len(dicc.values()) > 30)
 
     def test_sexmachine_features_list_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         fl = s.features_list()
         self.assertTrue(len(fl) > 20)
 
     def test_sexmachine_features_list_all_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         fl = s.features_list(all=True)
         self.assertTrue(len(fl) > 1000)
 
     def test_sexmachine_gender_list_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         gl = s.gender_list()
         self.assertEqual(gl, [1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1])
         self.assertEqual(len(gl), 21)
@@ -113,22 +113,22 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(s.unknown, 2)
 
     def test_sexmachine_gender_list_all_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         gl = s.gender_list(all=True)
         self.assertTrue(len(gl) > 1000)
 
     def test_sexmachine_accuracy_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         self.assertTrue(s.accuracy() > 0.5)
 
     def test_sexmachine_confusion_matrix_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         cm = s.confusion_matrix()
         am = np.array([[2, 1, 0],[3, 13, 0],[0, 2, 0]])
         self.assertTrue(np.array_equal(cm,am))
 
     def test_sexmachine_string2gender_method_returns_correct_result(self):
-        s = Sexmachine()
+        s = DameSexmachine()
         gender1 = s.string2gender("Arroyo Menéndez, David")
         gender2 = s.string2gender("xxxxx Laura")
         self.assertTrue(gender1, 'male')
@@ -136,21 +136,21 @@ class TddInPythonExample(unittest.TestCase):
 
 
     # def test_sexmachine_svc_list_method_returns_correct_result(self):
-    #     s = Sexmachine()
+    #     s = DameSexmachine()
     #     m = s.svc()
     #     predicted = m.predict([[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0, 0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0]])
     #     n = np.array([0])
     #     self.assertTrue(np.array_equal(predicted, n))
 
     # def test_sexmachine_sgd_list_method_returns_correct_result(self):
-    #     s = Sexmachine()
+    #     s = DameSexmachine()
     #     m = s.sgd()
     #     predicted = m.predict([[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0, 0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0]])
     #     n = np.array([1])
     #     self.assertTrue(np.array_equal(predicted, n))
 
     # def test_sexmachine_gaussianNB_list_method_returns_correct_result(self):
-    #     s = Sexmachine()
+    #     s = DameSexmachine()
     #     m = s.gaussianNB()
     #     array = [[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0,
     #                0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0],
@@ -162,7 +162,7 @@ class TddInPythonExample(unittest.TestCase):
 
 
     # def test_sexmachine_multinomialNB_list_method_returns_correct_result(self):
-    #     s = Sexmachine()
+    #     s = DameSexmachine()
     #     m = s.multinomialNB()
     #     array = [[ 0,  0,  1,  0, 21,  0,  0,  0,  0, 34,  2,  0,  0,  0,  0,  0,
     #                0,  0,  0,  5,  0,  0,  0,  0,  0,  2,  0,  0,  0, 34,  1,  0],
