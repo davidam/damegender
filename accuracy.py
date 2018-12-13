8f7bba21ef8f3023e2faf8e2fb25a2185fdf8da4
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-13 17:36:19 +0100

    accuracy.py: creation

---
+++ accuracy.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from app.dame_gender import Gender
+from app.dame_sexmachine import DameSexmachine
+from app.dame_namsor import DameNamsor
+from app.dame_genderguesser import DameGenderGuesser
+
+dn = DameNamsor()
+namsor_accuracy = dn.accuracy(path="files/partial.csv")
+print("Namsor accuracy: %s" % namsor_accuracy)
+
+
+
