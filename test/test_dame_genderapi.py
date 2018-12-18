6b29cb92723c33e654067b0b04d7568cac0029d2
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-18 08:45:49 +0100

    test/test_dame_genderapi.py: add argument binary in guess method

--- test/test_dame_genderapi.py
+++ test/test_dame_genderapi.py
@@ -5,5 +5,7 @@ class TddInPythonExample(unittest.TestCase):
 
     def test_genderapi_guess_method_returns_correct_result(self):
         dga = DameGenderApi()
-        g = dga.guess("Sara")
+        g = dga.guess("Sara", binary=False)
         self.assertEqual(g, "female")
+        g = dga.guess("Sara", binary=True)
+        self.assertEqual(g, 0)
