1cb2ded4f7f080da6b2febb2af9c14e1af3570aa
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-19 07:55:03 +0100

    confusion.py: creation

---
+++ confusion.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from app.dame_gender import Gender
+from app.dame_sexmachine import DameSexmachine
+from app.dame_namsor import DameNamsor
+from app.dame_genderguesser import DameGenderGuesser
+from app.dame_genderapi import DameGenderApi
+
+dn = DameNamsor()
+namsor_confusion_matrix = dn.confusion_matrix(path="files/partial.csv")
+print("Namsor confusion matrix:\n %s" % namsor_confusion_matrix)
+
+ds = DameSexmachine()
+sexmachine_confusion_matrix = ds.confusion_matrix(path="files/partial.csv")
+print("Sexmachine confusion matrix:\n %s" % sexmachine_confusion_matrix)
