c6fb1b629a1cced3f97cfbac96aa212414abc6c5
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-01-14 18:32:05 +0100

    app/dame_genderguesser.py, test/test_dame_genderguesser.py: adding guess_list method

--- test/test_dame_genderguesser.py
+++ test/test_dame_genderguesser.py
@@ -14,7 +14,12 @@ class TddInPythonExample(unittest.TestCase):
         g4 = dgg.guess("Laura", binary=True)
         self.assertEqual(g4, 0)
 
-    def test_dame_genderguesser_accuracy_method_returns_correct_result(self):
-        dgg = DameGenderGuesser()
-        self.assertTrue(dgg.accuracy(path="files/partial.csv") >= 0.5)
+    # def test_dame_genderguesser_accuracy_method_returns_correct_result(self):
+    #     dgg = DameGenderGuesser()
+    #     self.assertTrue(dgg.accuracy(path="files/partial.csv") >= 0.5)
 #        self.assertTrue(dgg.accuracy(path="files/all.csv") >= 0.5) # Comment assert, too much time
+
+    def test_dame_gender_guess_list_method_returns_correct_result(self):
+        dgg = DameGenderGuesser()
+        self.assertEqual(['male', 'male', 'male', 'male', 'male', 'male', 'female', 'female', 'male', 'male'], dgg.guess_list(path="files/partial.csv", binary=False)[0:10])
+        self.assertEqual([1, 1, 1, 1, 1, 1, 0, 0, 1, 1], dgg.guess_list(path="files/partial.csv",binary=True)[0:10])
