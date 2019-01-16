e6c09c8c74365a95b96a4be4fde1b4cf7f9053fa
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-01-16 16:43:24 +0100

    test/test_dame_genderapi.py: fix method names

--- test/test_dame_genderapi.py
+++ test/test_dame_genderapi.py
@@ -3,14 +3,14 @@ from app.dame_genderapi import DameGenderApi
 
 class TddInPythonExample(unittest.TestCase):
 
-    def test_genderapi_guess_method_returns_correct_result(self):
+    def test_dame_genderapi_guess_method_returns_correct_result(self):
         dga = DameGenderApi()
         g = dga.guess("Sara", binary=False)
         self.assertEqual(g, "female")
         g = dga.guess("Sara", binary=True)
         self.assertEqual(g, 0)
 
-    def test_dame_namsor_guess_list_method_returns_correct_result(self):
+    def test_dame_genderapi_guess_list_method_returns_correct_result(self):
         dga = DameGenderApi()
         self.assertEqual(['male', 'male', 'male', 'male', 'male', 'male', 'female', 'female', 'male', 'male', 'male', 'male', 'male', 'male', 'male', 'male', 'male', 'male', 'female', 'male', 'male'], dga.guess_list(path="files/partial.csv", binary=False))
         self.assertEqual([1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1], dga.guess_list(path="files/partial.csv",binary=True))
