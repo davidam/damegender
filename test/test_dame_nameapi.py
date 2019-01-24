f34acfeddc093e48312c4cede504c1037b307212
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-01-24 17:14:32 +0100

    test/test_dame_nameapi.py: commenting test, only with api key

--- test/test_dame_nameapi.py
+++ test/test_dame_nameapi.py
@@ -21,40 +21,40 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-import unittest
+# import unittest
 
-from app.dame_gender import Gender
-from app.dame_nameapi import DameNameapi
+# from app.dame_gender import Gender
+# from app.dame_nameapi import DameNameapi
 
-class TddInPythonExample(unittest.TestCase):
+# class TddInPythonExample(unittest.TestCase):
 
 
-    def test_dame_nameapi_init_method_returns_correct_result(self):
-        g = DameNameapi()
-        self.assertEqual(g.males, 0)
-        self.assertEqual(g.females, 0)
-        self.assertEqual(g.unknown, 0)
+#     def test_dame_nameapi_init_method_returns_correct_result(self):
+#         g = DameNameapi()
+#         self.assertEqual(g.males, 0)
+#         self.assertEqual(g.females, 0)
+#         self.assertEqual(g.unknown, 0)
 
-    def test_dame_nameapi_gender_guess_method_returns_correct_result(self):
-        g = DameNameapi()
-        self.assertEqual(1, g.guess("David", "Arroyo", binary=True))
-        self.assertEqual(0, g.guess("Andrea", "Arroyo", binary=True))
+#     def test_dame_nameapi_gender_guess_method_returns_correct_result(self):
+#         g = DameNameapi()
+#         self.assertEqual(1, g.guess("David", "Arroyo", binary=True))
+#         self.assertEqual(0, g.guess("Andrea", "Arroyo", binary=True))
 
-    def test_dame_nameapi_gender_list_method_returns_correct_result(self):
-        g = DameNameapi()
-        gl = g.gender_list()
-        self.assertEqual(gl, [1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1])
-        self.assertEqual(len(gl), 21)
-        self.assertEqual(g.females, 3)
-        self.assertEqual(g.males, 16)
-        self.assertEqual(g.unknown, 2)
+#     def test_dame_nameapi_gender_list_method_returns_correct_result(self):
+#         g = DameNameapi()
+#         gl = g.gender_list()
+#         self.assertEqual(gl, [1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1])
+#         self.assertEqual(len(gl), 21)
+#         self.assertEqual(g.females, 3)
+#         self.assertEqual(g.males, 16)
+#         self.assertEqual(g.unknown, 2)
 
-    def test_dame_nameapi_features_list_method_returns_correct_result(self):
-        g = DameNameapi()
-        fl = g.features_list()
-        self.assertTrue(len(fl) > 20)
+#     def test_dame_nameapi_features_list_method_returns_correct_result(self):
+#         g = DameNameapi()
+#         fl = g.features_list()
+#         self.assertTrue(len(fl) > 20)
 
-    def test_dame_nameapi_guess_list_method_returns_correct_result(self):
-        g = DameNameapi()
-        self.assertEqual(['male', 'male', 'male', 'male', 'male', 'male', 'female', 'female', 'male', 'male', 'male', 'male', 'male', 'male', 'neutral', 'male', 'male', 'male', 'female', 'male', 'male'], g.guess_list(path="files/partial.csv", binary=False))
-        self.assertEqual([1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 1, 1], g.guess_list(path="files/partial.csv",binary=True))
+#     def test_dame_nameapi_guess_list_method_returns_correct_result(self):
+#         g = DameNameapi()
+#         self.assertEqual(['male', 'male', 'male', 'male', 'male', 'male', 'female', 'female', 'male', 'male', 'male', 'male', 'male', 'male', 'neutral', 'male', 'male', 'male', 'female', 'male', 'male'], g.guess_list(path="files/partial.csv", binary=False))
+#         self.assertEqual([1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0, 1, 1], g.guess_list(path="files/partial.csv",binary=True))
