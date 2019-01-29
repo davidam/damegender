41df28ec4a7a05ffadc9031e4ec37dc7d7290ca6
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-01-29 18:27:02 +0100

    errors.py: creation

---
+++ errors.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from app.dame_gender import Gender
+from app.dame_sexmachine import DameSexmachine
+from app.dame_namsor import DameNamsor
+from app.dame_genderguesser import DameGenderGuesser
+from app.dame_genderapi import DameGenderApi
+from app.dame_genderize import DameGenderize
+from app.dame_nameapi import DameNameapi
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', default="files/min.csv")
+parser.add_argument('--api', default="all")
+args = parser.parse_args()
+#print(args.csv)
+
+
+
+
+if (args.api == "sexmachine"):
+    dn = DameSexmachine()
+    print("Sexmachine with %s has: " % args.csv)
+    gl1 = dn.gender_list(path=args.csv)
+    gl2 = dn.guess_list(path=args.csv, binary=True)
+    ec = dn.error_coded(gl1, gl2)
+    print("+ The error code: %s" % ec)
+    ecwa = dn.error_coded_without_na(gl1, gl2)
+    print("+ The error code without na: %s" %  ecwa)
+    naCoded = dn.na_coded(gl1, gl2)
+    print("+ The na coded: %s" %  naCoded)
+    egb = dn.error_gender_bias(gl1, gl2)
+    print("+ The error gender bias: %s" %  egb)
+elif (args.api == "nameapi"):
+    dn = DameNameapi()
+    print("Nameapi with %s has: " % args.csv)
+    gl1 = dn.gender_list(path=args.csv)
+    gl2 = dn.guess_list(path=args.csv, binary=True)
+    ec = dn.error_coded(gl1, gl2)
+    print("+ The error code: %s" % ec)
+    ecwa = dn.error_coded_without_na(gl1, gl2)
+    print("+ The error code without na: %s" %  ecwa)
+    naCoded = dn.na_coded(gl1, gl2)
+    print("+ The na coded: %s" %  naCoded)
+    egb = dn.error_gender_bias(gl1, gl2)
+    print("+ The error gender bias: %s" %  egb)
