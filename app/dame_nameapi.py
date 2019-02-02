4d3d77d88cf57769b5a60bfd18f0e8d10f7ad12d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-02 22:45:35 +0100

    app/dame_nameapi.py, test/test_dame_nameapi.py: using config file

--- app/dame_nameapi.py
+++ app/dame_nameapi.py
@@ -26,6 +26,7 @@ import requests
 import json
 import numpy as np
 import requests
+import configparser
 from app.dame_gender import Gender
 
 class DameNameapi(Gender):
@@ -33,60 +34,67 @@ class DameNameapi(Gender):
     def guess(self, name, surname, binary=False):
     # guess method to check names dictionary
         nameapilist = []
-        fichero = open("files/nameapipass.txt", "r+")
-        contenido = fichero.readline().rstrip()
-        #print(contenido)
+        config = configparser.RawConfigParser()
+        config.read('config.cfg')
+        if (config['DEFAULT']['nameapi'] == 'yes'):
+            fichero = open("files/nameapipass.txt", "r+")
+            contenido = fichero.readline().rstrip()
+            #print(contenido)
 
-        # url of the NameAPI.org endpoint:
-        url = (
-            "http://rc50-api.nameapi.org/rest/v5.0/parser/personnameparser?"
-            "apiKey="+contenido
-        )
+            # url of the NameAPI.org endpoint:
+            url = (
+                "http://rc50-api.nameapi.org/rest/v5.0/parser/personnameparser?"
+                "apiKey="+contenido
+            )
 
-        # Dict of data to be sent to NameAPI.org:
-        payload = {
-            "inputPerson": {
-                "type": "NaturalInputPerson",
-                "personName": {
-                    "nameFields": [
-                        {
-                            "string": name,
-                            "fieldType": "GIVENNAME"
-                        }, {
-                            "string": surname,
-                            "fieldType": "SURNAME"
-                        }
-                    ]
-                },
-                "gender": "UNKNOWN"
+            # Dict of data to be sent to NameAPI.org:
+            payload = {
+                "inputPerson": {
+                    "type": "NaturalInputPerson",
+                    "personName": {
+                        "nameFields": [
+                            {
+                                "string": name,
+                                "fieldType": "GIVENNAME"
+                            }, {
+                                "string": surname,
+                                "fieldType": "SURNAME"
+                            }
+                        ]
+                    },
+                    "gender": "UNKNOWN"
+                }
             }
-        }
 
-        # Proceed, only if no error:
-        try:
-            # Send request to NameAPI.org by doing the following:
-            # - make a POST HTTP request
-            # - encode the Python payload dict to JSON
-            # - pass the JSON to request body
-            # - set header's 'Content-Type' to 'application/json' instead of
-            #   default 'multipart/form-data'
-            resp = requests.post(url, json=payload)
-            resp.raise_for_status()
-            # Decode JSON response into a Python dict:
-            resp_dict = resp.json()
-            guess = resp_dict['matches'][0]['parsedPerson']['gender']['gender'].lower()
+            # Proceed, only if no error:
+            try:
+                # Send request to NameAPI.org by doing the following:
+                # - make a POST HTTP request
+                # - encode the Python payload dict to JSON
+                # - pass the JSON to request body
+                # - set header's 'Content-Type' to 'application/json' instead of
+                #   default 'multipart/form-data'
+                resp = requests.post(url, json=payload)
+                resp.raise_for_status()
+                # Decode JSON response into a Python dict:
+                resp_dict = resp.json()
+                guess = resp_dict['matches'][0]['parsedPerson']['gender']['gender'].lower()
+                if (binary == True):
+                    if (guess == 'female'):
+                        guess = 0
+                    elif (guess == 'male'):
+                        guess = 1
+                    else:
+                        guess = 2
+            except requests.exceptions.HTTPError as e:
+                print("Bad HTTP status code:", e)
+            except requests.exceptions.RequestException as e:
+                print("Network error:", e)
+        else:
             if (binary == True):
-                if (guess == 'female'):
-                    guess = 0
-                elif (guess == 'male'):
-                    guess = 1
-                else:
-                    guess = 2
-        except requests.exceptions.HTTPError as e:
-            print("Bad HTTP status code:", e)
-        except requests.exceptions.RequestException as e:
-            print("Network error:", e)
-
+                guess = 2
+            else:
+                guess = "unknown"
         return guess
 
     def guess_list(self, path='files/partial.csv', binary=False):
