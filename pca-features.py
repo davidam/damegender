ab95e3766328f4fd735ee1f95ec29bfe9ccf697a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-21 15:35:24 +0100

    pca-features.py: now show the matrix between features and principal components

--- pca-features.py
+++ pca-features.py
@@ -24,6 +24,8 @@
 
 from pprint import pprint
 import pandas as pd
+from sklearn.decomposition import PCA
+
 import matplotlib.pyplot as plt
 from app.dame_sexmachine import DameSexmachine
 from app.dame_gender import Gender
@@ -41,47 +43,41 @@ print(x.columns)
 y = g.dataset2genderlist(dataset="files/all.csv")
 print(y)
 
-print("STEP2: STANDARIZE THE DATA")
-from sklearn.preprocessing import StandardScaler
-# Standardizing the features
-x = StandardScaler().fit_transform(x)
+# load dataset
 
-# ## PCA PROJECTION TO N DIMENSIONS
+# normalize data
+from sklearn import preprocessing
+data_scaled = pd.DataFrame(preprocessing.scale(x),columns = x.columns)
 
-from sklearn.decomposition import PCA
+# PCA
 pca = PCA(n_components=6)
-principalComponents = pca.fit_transform(x)
-print("STEP3: PCA PROJECTION")
-pprint(principalComponents)
-principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2', 'principal component 3', 'principal component 4', 'principal component 5', 'principal component 6'])
+pca.fit_transform(data_scaled)
 
-target = pd.DataFrame(data = y, columns = ['target component'])
+# Dump components relations with features:
+print(pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2','PC-3','PC-4','PC-5','PC-6']))
 
-print(principalDf.join(target))
 
-# print(list(principalDf.columns.values))
-# finalDf = pd.concat([principalDf, y])
 
-# ## Visualize N Projection
+# print("STEP2: STANDARIZE THE DATA")
+# from sklearn.preprocessing import StandardScaler
+# # Standardizing the features
+# x = StandardScaler().fit_transform(x)
 
-# fig = plt.figure(figsize = (8,8))
-# ax = fig.add_subplot(1,1,1)
-# ax.set_alabel('Principal Component 1', fontsize = 15)
-# ax.set_blabel('Principal Component 2', fontsize = 15)
-# ax.set_clabel('Principal Component 3', fontsize = 15)
-# ax.set_dlabel('Principal Component 4', fontsize = 15)
-# ax.set_elabel('Principal Component 5', fontsize = 15)
-# ax.set_flabel('Principal Component 6', fontsize = 15)
-# ax.set_title('6 component PCA', fontsize = 20)
-# targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-# colors = ['r', 'g', 'b']
-# for target, color in zip(targets,colors):
-#     indicesToKeep = finalDf['target'] == target
-#     ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-#                , finalDf.loc[indicesToKeep, 'principal component 2']
-#                , c = color
-#                , s = 50)
-# ax.legend(targets)
-# ax.grid()
+# # ## PCA PROJECTION TO N DIMENSIONS
 
+# from sklearn.decomposition import PCA
+# pca = PCA(n_components=6)
+# principalComponents = pca.fit_transform(x)
+# print("STEP3: PCA PROJECTION")
+# pprint(principalComponents)
+# principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2', 'principal component 3', 'principal component 4', 'principal component 5', 'principal component 6'])
+
+# target = pd.DataFrame(data = y, columns = ['target component'])
+
+# finalDf = data_scaled.join(target)
+
+
+# # # ## Visualize N Projection
+
+# finalDf.plot()
 # plt.show()
