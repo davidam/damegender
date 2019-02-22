adde58223d1d31a254ebfeafb0ef1671e469b52d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-22 17:35:29 +0100

    app/dame_customsearch.py, test/test_dame_customsearch.py: creation

---
+++ test/test_dame_customsearch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import unittest
+from googleapiclient.discovery import build
+from app.dame_gender import Gender
+from app.dame_customsearch import DameCustomsearch
+
+class TddInPythonExample(unittest.TestCase):
+
+    def test_dame_customsearch_method_returns_correct_result(self):
+        g = DameCustomsearch()
+        self.assertEqual(1, g.guess("David", binary=True))
