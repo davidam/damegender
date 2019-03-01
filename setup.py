5b13e8d2ae440a3a56cb6cf3fd374ab5438bdc03
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-03-01 13:30:08 +0100

    setup.py: now the package is including directories

--- setup.py
+++ setup.py
@@ -1,17 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# def readme():
+#     with open('README.org') as f:
+#         return f.read()
 
 setup(name='damegender',
-      version='0.1.2',
+      version='0.0.9',
       description='Gender Detection Tool by David Arroyo MEnéndez',
+      long_description='Gender Detection Tool by David Arroyo MEnéndez',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent",
+      ],
+      keywords='gender repositories',
       url='http://github.com/davidam/damegender',
       author='David Arroyo Menéndez',
       author_email='davidam@gnu.org',
       license='GPLv3',
-      packages=['damegender'],
+      packages=['damegender', 'damegender.app', 'damegender.test'],
+      package_dir={'damegender': 'src/damegender', 'damegender.app': 'src/damegender/app', 'damegender.test': 'src/damegender/test', 'damegender.files': 'src/damegender/files'},
+      package_data={'damegender.files': ['files/*']},
       install_requires=[
+          'markdown',
           'nltk',
           'perceval',
           'requests',
@@ -24,9 +58,10 @@ setup(name='damegender',
           'pandas',
           'matplotlib',
       ],
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: GPLv3 License",
-          "Operating System :: OS Independent",
-      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['funniest-joke=funniest.command_line:main'],
+      },
+      include_package_data=True,
       zip_safe=False)
