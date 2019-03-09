2672b484b17dab29fbccb319ee4ed6cbb6ca8f13
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-03-09 14:47:10 +0100

    setup.py: add methods

--- setup.py
+++ setup.py
@@ -21,16 +21,45 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-
+import os
+import re
 from setuptools import setup
 
-# def readme():
-#     with open('README.org') as f:
-#         return f.read()
 
+cwd = os.getcwd()
+
+def files_one_level(directory):
+    f = os.popen('find '+ directory )
+    l = []
+    for line in f:
+        fields = line.strip().split()
+        l.append(fields[0])
+    return l
+
+def files_one_level_drop_pwd(directory):
+    f = os.popen('find '+ directory)
+    l = []
+    for line in f:
+        fields = line.strip().split()
+        if not(os.path.isdir(fields[0])) and ("__init__.py" not in fields[0]):
+            l.append(drop_pwd(fields[0]))
+    return l
+
+def drop_pwd(s):
+    cwd = os.getcwd()
+    result = ""
+    if re.search(cwd, s):
+        result = re.sub(cwd+'/', '', s)
+    return result
+
+
+
+#files_one_level_drop_pwd
+#print(du.files_one_level_drop_pwd("~/git/damegender/src/damegender/files"))
+#print(files_one_level_drop_pwd(cwd+"/src/damegender/files"))
 
 setup(name='damegender',
-      version='0.0.24',
+      version='0.0.28',
       description='Gender Detection Tool by David Arroyo MEnéndez',
       long_description='Gender Detection Tool by David Arroyo MEnéndez',
       classifiers=[
@@ -47,6 +76,10 @@ setup(name='damegender',
       package_dir={'damegender': 'src/damegender', 'damegender.app': 'src/damegender/app', 'damegender.test': 'src/damegender/test', 'damegender.files': 'src/damegender/files'},
       data_files=[('damegender', ['src/damegender/files/features_list.csv', 'src/damegender/files/features_list_cat.csv', 'src/damegender/files/features_list_no_cat.csv', 'src/damegender/files/names/all.csv', 'src/damegender/files/names/partial.csv', 'src/damegender/files/names/names_es/femeninos_original.csv', 'src/damegender/files/names/names_es/masculinos_original.csv', 'src/damegender/files/names/names_es/masculinos.txt', 'src/damegender/files/names/names_es/femeninos.txt'])],
 #      data_files=[('damegender', ['src/damegender/files/*csv', 'src/damegender/files/datamodels/*', 'src/damegender/files/images/*', 'src/damegender/files/names/*', 'src/damegender/files/names/names_es/*'])],
+#      data_files=[('damegender', files_one_level_drop_pwd(cwd+"/src/damegender/files"))],
+#      data_files=[('damegender', ['src/damegender/files/*csv', 'src/damegender/files/datamodels/*', 'src/damegender/files/images/*', 'src/damegender/files/names/*', 'src/damegender/files/names/names_es/*'])],
+
+
       install_requires=[
           'markdown',
           'nltk',
