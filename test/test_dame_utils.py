7ab1602d95d4f1fb954054697f7cc03deb176fed
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-04-26 22:33:56 +0200

    test/test_dame_utils.py: not src folder, yet

--- test/test_dame_utils.py
+++ test/test_dame_utils.py
@@ -82,4 +82,4 @@ class TddInPythonExample(unittest.TestCase):
     def test_dame_utils_files_one_level_drop_pwd_method_returns_correct_result(self):
         du = DameUtils()
         cwd = os.getcwd()
-        self.assertEqual(sorted(du.files_one_level_drop_pwd(cwd+"/src/damegender/files/datamodels")), ['src/damegender/files/datamodels/bernoulliNB_model.sav', 'src/damegender/files/datamodels/gaussianNB_model.sav', 'src/damegender/files/datamodels/multinomialNB_model.sav', 'src/damegender/files/datamodels/sgd_model.sav', 'src/damegender/files/datamodels/svc_model.sav'])
+        self.assertEqual(sorted(du.files_one_level_drop_pwd(cwd+"/files/datamodels")), ['files/datamodels/bernoulliNB_model.sav', 'files/datamodels/gaussianNB_model.sav', 'files/datamodels/multinomialNB_model.sav', 'files/datamodels/sgd_model.sav', 'files/datamodels/svc_model.sav'])
