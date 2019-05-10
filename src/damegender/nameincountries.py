f350b911fce8d95fc90e7ebc8a0b12135f432bab
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-10 19:16:16 +0200

    nameincountries.py: it found the name as part in a composed name. Now, it finds the name as the exact name

--- src/damegender/nameincountries.py
+++ src/damegender/nameincountries.py
@@ -22,7 +22,7 @@
 # Boston, MA 02110-1301 USA,
 
 # From a name give a list of countries where the people is using the name
-
+import re
 import os
 import csv
 import sys
@@ -33,9 +33,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("name", help="display the country")
 args = parser.parse_args()
 
-# TODO: This script file found name as part in a composed name. It must find the name as the exact name
-
-# Diccionario de nombres de paises y números en nam_dict.txt
+# Diccionary of countries and person names in files/names/nam_dict.txt
 keyscountries = {"30": "Great Britain", "31": "Ireland", "32": "USA", "33": "Italy", "34": "Malta", "35": "Portugal", "36": "Spain", "37": "France", "38": "Belgium", "39": "Luxembourg", "40": "The Netherlands", "41": "East Frisia", "42": "Germany", "43": "Austria", "44": "Swiss", "45": "Iceland", "46": "Denmark", "47": "Norway", "48": "Sweden", "49": "Finland", "50": "Estonia", "51": "Latvia", "52": "Lithuania", "53": "Poland", "54": "Czech Republic", "55": "Slovakia", "56": "Hungary", "57": "Romania", "58": "Bulgaria", "59": "Bosnia and Herzegovina", "60": "Croatia", "61": "Kosovo", "62": "Macedonia", "63": "Montenegro", "64": "Serbia", "65": "Slovenia", "66": "Albania", "67": "Greece", "68": "Rusia", "69": "Belarus", "70": "Moldova", "71": "Ukraine", "72": "Armenia", "73": "Azerbaijan", "74": "Georgia", "75": "Kazakhstan/Uzbekistan", "76": "Turkey", "77": "Arabia/Persia", "78": "Israel", "79": "China", "80": "India/Sri Lanka", "81": "Japan", "82": "Korea", "83": "Vietnam", "84": "other countries"}
 
 du = DameUtils()
@@ -61,15 +59,22 @@ if (len(sys.argv) > 1):
     both = [ ]
 
     for rowres in results:
-        #print(rowres[0].title())
-        for i in range(30, 84):
-            if (exists_in_country(int(i), rowres)):
-                if (rowres[0].title() == "M"):
-                    males.append(keyscountries[str(i)])
-                elif (rowres[0].title() == "F"):
-                    females.append(keyscountries[str(i)])
-                elif (rowres[0].title() == "="):
-                    both.append(keyscountries[str(i)])
+        regex = "(M|F|=|\?|1)( |M|F)?( )(\w+)( )?(\w+)?( )?(\w+)?"
+        n = re.match(regex, rowres)
+        if (n.group(6)):
+            string = n.group(4) + n.group(5) + str(n.group(6))
+        else:
+            string = n.group(4) + n.group(5) 
+
+        if (du.drop_white_space(args.name) == du.drop_white_space(string) ):
+            for i in range(30, 84):
+                if (exists_in_country(int(i), rowres)):
+                    if (rowres[0].title() == "M"):
+                        males.append(keyscountries[str(i)])
+                    elif (rowres[0].title() == "F"):
+                        females.append(keyscountries[str(i)])
+                    elif (rowres[0].title() == "="):
+                        both.append(keyscountries[str(i)])
 
     print("males: " + str(males))
     print("females: " + str(females))
