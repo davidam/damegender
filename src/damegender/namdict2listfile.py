319cc488bc6dbcb17b1f988e4c0ef5fbd8028dab
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-30 18:45:09 +0200

    namdict2listfile.py: add filenamdict2list, envolve source in namdict2file

--- src/damegender/namdict2listfile.py
+++ src/damegender/namdict2listfile.py
@@ -21,20 +21,37 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 import os
+import csv
+csv.field_size_limit(3000000)
 
-filepath = 'files/names/nam_dict.txt'
-mylist = []
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
+def namdict2file():
+    filepath = 'files/names/nam_dict.txt'
+    mylist = []
+    with open(filepath) as fp:
+        for cnt, line in enumerate(fp):
         # From 3 to 25
-        if (cnt > 292):
-            name = ""
-            for i in range(3,25):
-                name = name + str(line[i])
-            mylist += [name]
-print(mylist)
+            if (cnt > 292):
+                name = ""
+                for i in range(3,25):
+                    name = name + str(line[i])
+                mylist += [name]
+    print(mylist)
+    file = open("files/names/nam_dict_list.txt", "w")
+    file.writelines(mylist)
+    file.close()
 # cmd1 = 'echo '+ str(mylist) +' > mylist.txt'
 # print(os.system(cmd1))
-file = open("files/names/nam_dict_list.txt", "w")
-file.writelines(mylist)
-file.close()
+
+
+def filenamdict2list():
+    names = [ ]
+    dataset='files/names/nam_dict_list.txt'
+    with open('files/names/nam_dict_list.txt', 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        for row in reader:
+            names = names + row[0].split()
+    return names
+
+namdict2file()
+print(filenamdict2list()[0])
+#print(len(filenamdict2list()))
