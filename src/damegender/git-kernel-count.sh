088e88bfbe693183616c39c650368325ba94971e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-06-09 18:02:04 +0200

    src/damegender/git-kernel-count.sh: moved from root

---
+++ src/damegender/git-kernel-count.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+#read file
+
+
+i=0
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    printf "##### %s ######\n" "$line"
+    python3 git2gender.py $line --directory="/tmp/clonedir$i"
+	i=`expr $i + 1`
+done < files/kernelgits.txt
