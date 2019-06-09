7b235db99c919f054e7856608dfbe1f554029c33
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-06-09 09:00:10 +0200

    performance.sh: creation

---
+++ src/damegender/performance.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+echo "These accuracies is only for damegender and genderguesser. To avoid problems with apikeys"
+time python3 accuracy.py --api="damegender" --csv="files/names/all.csv"
+time python3 accuracy.py --api="genderguesser" --csv="files/names/all.csv"
