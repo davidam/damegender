8ba77ae134d5b3ee5e0a751b4302acdefddf5536
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-06-10 17:38:03 +0200

    infofeatures.py: more about first letter

--- src/damegender/infofeatures.py
+++ src/damegender/infofeatures.py
@@ -108,7 +108,10 @@ for i in g.females_list():
     if i[0].lower() in 'abcdefghijklmnñopqrstuvwxyz':
         diccfemales[i[0].lower()] = diccfemales[i[0].lower()] + 1
 print("there are %s females" % len(g.females_list()))
-print("first letter females dictionary: " + str(diccfemales))
+print("first letter females dictionary: ")
+listtuples = sorted(diccfemales.items(), reverse=True,  key=lambda x: x[1])
+for elem in listtuples :
+    print(elem[0] , " ::" , (elem[1] / len(g.females_list())))
 
 print("---------------------------------------------------------------")
 diccmales = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g":0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "ñ": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z":0 }
@@ -117,38 +120,41 @@ for i in g.males_list():
     if i[0].lower() in 'abcdefghijklmnñopqrstuvwxyz':
         diccmales[i[0].lower()] = diccmales[i[0].lower()] + 1
 print("there are %s males" % len(g.males_list()))
-print("first letter males dictionary: " + str(diccmales))
-
+print("first letter males dictionary: ")
+listtuples = sorted(diccmales.items(), reverse=True,  key=lambda x: x[1])
+for elem in listtuples :
+    print(elem[0] , " ::" , (elem[1] / len(g.males_list())))
+#print(sorted(diccmales.iteritems()))
 
 # diccmales = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "h": 0, "i": 0}
 # print(dicc.values())
 # print(dicc.keys())
 # print(dicc["a"])
 
-# # FIRST LETTER CONSONANT
-# print("---------------------------------------------------------------")
-# females = 0
-# for i in g.females_list():
-#     if (g.features_int(i)["first_letter_consonant"] == 1):
-#         females = females + 1
-# print("Females with first letter consonant: %s" % (females/ len(g.females_list())))
+# FIRST LETTER CONSONANT
+print("---------------------------------------------------------------")
+females = 0
+for i in g.females_list():
+    if (g.features_int(i)["first_letter_consonant"] == 1):
+        females = females + 1
+print("Females with first letter consonant: %s" % (females/ len(g.females_list())))
 
-# males = 0
-# for i in g.males_list():
-#     if (g.features_int(i)["first_letter_consonant"] == 1):
-#         males = males + 1
-# print("Males with first letter consonant: %s" % (males/ len(g.males_list())))
+males = 0
+for i in g.males_list():
+    if (g.features_int(i)["first_letter_consonant"] == 1):
+        males = males + 1
+print("Males with first letter consonant: %s" % (males/ len(g.males_list())))
 
-# # FIRST LETTER VOCAL
-# print("---------------------------------------------------------------")
-# females = 0
-# for i in g.females_list():
-#     if (g.features_int(i)["first_letter_vocal"] == 1):
-#         females = females + 1
-# print("Females with first letter vocal: %s" % (females/ len(g.females_list())))
+# FIRST LETTER VOCAL
+print("---------------------------------------------------------------")
+females = 0
+for i in g.females_list():
+    if (g.features_int(i)["first_letter_vocal"] == 1):
+        females = females + 1
+print("Females with first letter vocal: %s" % (females/ len(g.females_list())))
 
-# males = 0
-# for i in g.males_list():
-#     if (g.features_int(i)["first_letter_vocal"] == 1):
-#         males = males + 1
-# print("Males with first letter vocal: %s" % (males/ len(g.males_list())))
+males = 0
+for i in g.males_list():
+    if (g.features_int(i)["first_letter_vocal"] == 1):
+        males = males + 1
+print("Males with first letter vocal: %s" % (males/ len(g.males_list())))
