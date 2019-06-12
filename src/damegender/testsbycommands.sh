91c50266cd0d54417ec4d9384c7fb70531e2d575
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-06-12 11:29:34 +0200

    testsbycommands.sh: %y by %Y

--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -20,81 +20,81 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-python3 main.py David > files/tests/maindavid-$(date "+%y-%m-%d-%H").txt
+python3 main.py David > files/tests/maindavid-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/maindavid.txt files/tests/maindavid-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/maindavid.txt files/tests/maindavid-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
     echo "maindavid test is failing"
 else
     echo "maindavid test is ok"
 fi
 
-python3 main.py Inés > files/tests/mainines-$(date "+%y-%m-%d-%H").txt
+python3 main.py Inés > files/tests/mainines-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainines.txt files/tests/mainines-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainines.txt files/tests/mainines-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainines test is failing"
 else
 	echo "mainines test is ok"
 fi
 
-python3 main.py Alex > files/tests/mainalex-$(date "+%y-%m-%d-%H").txt
+python3 main.py Alex > files/tests/mainalex-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainalex.txt files/tests/mainalex-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainalex.txt files/tests/mainalex-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainalex test is failing"
 else
 	echo "mainalex test is ok"
 fi
 
-python3 main.py Andrea > files/tests/mainandrea-$(date "+%y-%m-%d-%H").txt
+python3 main.py Andrea > files/tests/mainandrea-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainandrea.txt files/tests/mainandrea-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainandrea.txt files/tests/mainandrea-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainandrea test is failing"
 else
 	echo "mainandrea test is ok"
 fi
 
-python3 main.py "Jesús María" > files/tests/mainjesusmaria-$(date "+%y-%m-%d-%H").txt
+python3 main.py "Jesús María" > files/tests/mainjesusmaria-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainjesusmaria.txt files/tests/mainjesusmaria-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainjesusmaria.txt files/tests/mainjesusmaria-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainjesusmaria test is failing"
 else
 	echo "mainjesusmaria test is ok"
 fi
 
-python3 main.py "José María" > files/tests/mainjosemaria-$(date "+%y-%m-%d-%H").txt
+python3 main.py "José María" > files/tests/mainjosemaria-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainjosemaria.txt files/tests/mainjosemaria-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainjosemaria.txt files/tests/mainjosemaria-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainjosemaria test is failing"
 else
 	echo "mainjosemaria test is ok"
 fi
 
-python3 main.py "Jesús" --total=genderguesser > files/tests/mainjesusgenderguesser-$(date "+%y-%m-%d-%H").txt
+python3 main.py "Jesús" --total=genderguesser > files/tests/mainjesusgenderguesser-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainjesusgenderguesser.txt files/tests/mainjesusgenderguesser-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainjesusgenderguesser.txt files/tests/mainjesusgenderguesser-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainjesusgenderguesser test is failing"
 else
 	echo "mainjesusgenderguesser test is ok"
 fi
 
-python3 main.py "Sara" --total=genderguesser > files/tests/mainsaragenderguesser-$(date "+%y-%m-%d-%H").txt
+python3 main.py "Sara" --total=genderguesser > files/tests/mainsaragenderguesser-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainsaragenderguesser.txt files/tests/mainsaragenderguesser-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainsaragenderguesser.txt files/tests/mainsaragenderguesser-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainsaragenderguesser test is failing"
 else
 	echo "mainsaragenderguesser test is ok"
 fi
 
-python3 main.py sara --total=genderguesser > files/tests/mainsara2genderguesser-$(date "+%y-%m-%d-%H").txt
+python3 main.py sara --total=genderguesser > files/tests/mainsara2genderguesser-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainsara2genderguesser.txt files/tests/mainsara2genderguesser-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainsara2genderguesser.txt files/tests/mainsara2genderguesser-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "mainsara2genderguesser test is failing"
 else
@@ -102,54 +102,54 @@ else
 fi
 
 
-python3 main.py Mesa --ml=nltk > files/tests/mainmesa-$(date "+%y-%m-%d-%H").txt
+python3 main.py Mesa --ml=nltk > files/tests/mainmesa-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/mainmesa.txt files/tests/mainmesa-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/mainmesa.txt files/tests/mainmesa-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
     echo "mainmesa test is failing"
 else
     echo "mainmesa test is ok"
 fi
 
-python3 nameincountries.py David > files/tests/nameincountriesdavid-$(date "+%y-%m-%d-%H").txt
+python3 nameincountries.py David > files/tests/nameincountriesdavid-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/nameincountriesdavid.txt files/tests/nameincountriesdavid-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/nameincountriesdavid.txt files/tests/nameincountriesdavid-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "nameincountries test is failing"
 else
 	echo "nameincountries test is ok"
 fi
 
-python3 accuracy.py --csv=files/names/min.csv > files/tests/accuracymin-$(date "+%y-%m-%d-%H").txt
+python3 accuracy.py --csv=files/names/min.csv > files/tests/accuracymin-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/accuracymin.txt files/tests/accuracymin-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/accuracymin.txt files/tests/accuracymin-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "accuracymin test is failing"
 else
 	echo "accuracymin test is ok"
 fi
 
-python3 accuracy.py --api="genderguesser" --csv=files/names/min.csv > files/tests/accuracygenderguesser-$(date "+%y-%m-%d-%H").txt
+python3 accuracy.py --api="genderguesser" --csv=files/names/min.csv > files/tests/accuracygenderguesser-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/accuracygenderguesser.txt files/tests/accuracygenderguesser-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/accuracygenderguesser.txt files/tests/accuracygenderguesser-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "accuracygenderguesser test is failing"
 else
 	echo "accuracygenderguesser test is ok"
 fi
 
-python3 confusion.py --api="damegender" --dimensions=3x2 --csv=files/names/min.csv > files/tests/confusiondamegender-$(date "+%y-%m-%d-%H").txt
+python3 confusion.py --api="damegender" --dimensions=3x2 --csv=files/names/min.csv > files/tests/confusiondamegender-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/confusiondamegender.txt files/tests/confusiondamegender-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/confusiondamegender.txt files/tests/confusiondamegender-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "confusiondamegender test is failing"
 else
 	echo "confusiondamegender test is ok"
 fi
 
-python3 confusion.py --api="genderguesser" --dimensions=3x2 --csv=files/names/min.csv > files/tests/confusiongenderguesser-$(date "+%y-%m-%d-%H").txt
+python3 confusion.py --api="genderguesser" --dimensions=3x2 --csv=files/names/min.csv > files/tests/confusiongenderguesser-$(date "+%Y-%m-%d-%H").txt
 
-if ! cmp files/tests/confusiongenderguesser.txt files/tests/confusiongenderguesser-$(date "+%y-%m-%d-%H").txt >/dev/null 2>&1
+if ! cmp files/tests/confusiongenderguesser.txt files/tests/confusiongenderguesser-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
 	echo "confusiongenderguesser test is failing"
 else
