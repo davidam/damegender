a45380e892898574482487f8c76d71a800304cb5
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-06-30 23:02:31 +0200

    pca-components.py: add copyright, add savefig

--- src/damegender/pca-components.py
+++ src/damegender/pca-components.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 import numpy as np
 from sklearn.decomposition import PCA
@@ -5,9 +27,13 @@ from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import argparse
 import sys
+from app.dame_utils import DameUtils
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--csv')
 args = parser.parse_args()
+
+du = DameUtils()
 
 if (len(sys.argv) > 1):
 #filepath = 'files/features_list.csv' #your path here
@@ -24,6 +50,7 @@ if (len(sys.argv) > 1):
     plt.xlabel('Number of Components')
     plt.ylabel('Variance (%)') #for each component
     plt.title('Dataset Explained Variance')
+    plt.savefig('files/images/pca_components_'+ str(du.path2file(args.csv)) + '.png')
     plt.show()
 else:
     print("You must introduce a csv file.")
