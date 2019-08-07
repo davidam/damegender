414ef26915256f0b02cf434fcd6b6cd2719edd18
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-08-08 00:37:05 +0200

    pca-features.py: indenting and add one choice

--- src/damegender/pca-features.py
+++ src/damegender/pca-features.py
@@ -34,69 +34,73 @@ from app.dame_gender import Gender
 
 ## PARAMETERS
 parser = argparse.ArgumentParser()
-parser.add_argument("--categorical", default="both", choices=['both', 'noletters', 'nocategorical'])
-parser.add_argument("--components", type=int)
+parser.add_argument("--categorical", default="both", choices=['both', 'noletters', 'nocategorical', 'all'])
+parser.add_argument("--components", default=0, type=int)
 args = parser.parse_args()
 
+if (args.components > 0):
 ## LOAD DATASET
-g = Gender()
-if (args.categorical == "both"):
-    g.features_list2csv(categorical="both", path="files/names/allnoundefined.csv")
-    features = "files/features_list_no_undefined.csv"
-elif (args.categorical == "noletters"):
-    g.features_list2csv(categorical="noletters", path="files/names/allnoundefined.csv")
-    features = "files/features_list_cat.csv"
-elif (args.categorical == "nocategorical"):
-    g.features_list2csv(categorical="nocategorical", path="files/names/allnoundefined.csv")
-    features = "files/features_list_no_cat.csv"
+    g = Gender()
+    if (args.categorical == "both"):
+        g.features_list2csv(categorical="both", path="files/names/allnoundefined.csv")
+        features = "files/features_list_no_undefined.csv"
+    elif (args.categorical == "noletters"):
+        g.features_list2csv(categorical="noletters", path="files/names/allnoundefined.csv")
+        features = "files/features_list_cat.csv"
+    elif (args.categorical == "nocategorical"):
+        g.features_list2csv(categorical="nocategorical", path="files/names/allnoundefined.csv")
+        features = "files/features_list_no_cat.csv"
+    else:
+        g.features_list2csv(categorical="both", path="files/names/all.csv")
+        features = "files/features_list.csv"
+    ## STEP1: N COMPONENTS + 1 TARGET
+    x = pd.read_csv(features)
+    #print(x.columns)
+
+    y = g.dataset2genderlist(dataset="files/names/allnoundefined.csv")
+    #print(y)
+
+    # STEP2: ADDING TARGET
+    target = pd.DataFrame(data = y, columns = ['target component'])
+    finalDf = x.join(target)
 
 
-## STEP1: N COMPONENTS + 1 TARGET
-
-x = pd.read_csv(features)
-#print(x.columns)
-
-y = g.dataset2genderlist(dataset="files/names/allnoundefined.csv")
-#print(y)
-
-# STEP2: ADDING TARGET
-target = pd.DataFrame(data = y, columns = ['target component'])
-finalDf = x.join(target)
+    # STEP3: NORMALIZE DATA
+    from sklearn import preprocessing
+    data_scaled = pd.DataFrame(preprocessing.scale(finalDf), columns = finalDf.columns)
 
 
-# STEP3: NORMALIZE DATA
-from sklearn import preprocessing
-data_scaled = pd.DataFrame(preprocessing.scale(finalDf), columns = finalDf.columns)
+    # STEP4: PCA
+    pca = PCA(n_components=int(args.components))
+    pca.fit_transform(data_scaled)
 
+    # STEP5: Dump components relations with features:
 
-# STEP4: PCA
-pca = PCA(n_components=int(args.components))
-pca.fit_transform(data_scaled)
+    finalIndex = []
+    for i in range(1, int(args.components)+1):
+        finalIndex.append('PC-'+str(i))
+    #print(finalIndex)
 
-# STEP5: Dump components relations with features:
+    #finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2','PC-3','PC-4','PC-5','PC-6'])
+    finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = finalIndex)
 
-finalIndex = []
-for i in range(1, int(args.components)+1):
-    finalIndex.append('PC-'+str(i))
-#print(finalIndex)
+    jsondata = finalDf.to_json(orient='records')
 
-#finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2','PC-3','PC-4','PC-5','PC-6'])
-finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = finalIndex)
+    fo = open("files/pca.json", "w")
+    fo.write(jsondata);
+    # Close json file
+    fo.close()
 
-jsondata = finalDf.to_json(orient='records')
+    print("The json file is created in files/pca.json")
 
-fo = open("files/pca.json", "w")
-fo.write(jsondata);
-# Close json file
-fo.close()
+    # STEP6: Dump to html file
 
-print("The json file is created in files/pca.json")
+    jh = json2html.convert(json = jsondata)
+    jh = "<html><body>" + jh + "</body></html>"
+    fo = open("files/pca.html", "w")
+    fo.write(jh);
 
-# STEP6: Dump to html file
-
-jh = json2html.convert(json = jsondata)
-jh = "<html><body>" + jh + "</body></html>"
-fo = open("files/pca.html", "w")
-fo.write(jh);
-
-print("The html file is created in files/pca.html")
+    print("The html file is created in files/pca.html")
+else:
+    print("Components must be >0, try with: $ python3 pca-features.py --categorical='noletters' --components=3")
+    print("You can use $ python3 pca-components.py to determine the number of components")
