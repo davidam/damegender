b3c8973e28beab184a52ad276a6490bf7861c453
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-01-29 22:12:54 +0100

    damegender2json.py: creation

---
+++ src/damegender/damegender2json.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from app.dame_gender import Gender
+from app.dame_sexmachine import DameSexmachine
+from app.dame_namsor import DameNamsor
+from app.dame_genderguesser import DameGenderGuesser
+from app.dame_genderapi import DameGenderApi
+from app.dame_genderize import DameGenderize
+from app.dame_nameapi import DameNameapi
+from app.dame_customsearch import DameCustomsearch
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', type=str, required=True, help="files/names/min.csv")
+#parser.add_argument('--gendercase', type=str, required=True, help="files/names/min.csv")
+parser.add_argument('--ml', default="nltk", choices=['nltk', 'svc', 'sgd', 'gaussianNB', 'multinomialNB', 'bernoulliNB'])
+args = parser.parse_args()
+
+ds = DameSexmachine()
+print("################### Dame Gender!!")
+gl = ds.gender_list(path=args.csv)
+print("Gender list: " + str(gl))
+sl = ds.guess_list(path=args.csv, binary=True)
+print("Guess list:  " +str(sl))
+
+ds.csv2json(path=args.csv, l=sl, jsonf=args.csv +".json")
