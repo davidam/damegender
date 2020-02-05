3cad8a165a52268e73b21786a46b2bc6ab026763
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-02-05 23:26:03 +0100

    app/dame_utils.py: fixes in yes_or_not

--- src/damegender/app/dame_utils.py
+++ src/damegender/app/dame_utils.py
@@ -167,11 +167,14 @@ class DameUtils():
                 l.append(self.drop_pwd(fields[0]))
         return l
 
-    def yes_or_not(question):
-        reply = str(raw_input(question+' (y/n): ')).lower().strip()
-        if reply[0] == 'y':
-            return True
-        if reply[0] == 'n':
-            return False
+    def yes_or_not(self, question):
+
+        reply = str(input(question+' (y/n): ')).lower().strip()
+        ret = None
+        if ((reply[0] == 'y') or (reply[0] == 'yes')):
+            ret = True
+        elif ((reply[0] == 'n') or (reply[0] == 'no') or (reply[0] == 'not')):
+            ret = False
         else:
-            return yes_or_not("Uhhhh... please enter ")
+            ret = self.yes_or_not("Uhhhh... please enter ")
+        return ret
