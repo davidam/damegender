43ded5a4c943d438469e8f046f0fadecb4844ded
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-04-05 00:23:51 +0200

    app/dame_nltk.py test/test_dame_nltk.py: creation

---
+++ src/damegender/test/test_dame_nltk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import unittest
+import os
+from app.dame_nltk import DameNLTK
+
+class TddInPythonExample(unittest.TestCase):
+
+        def test_sentence_similarity(self):
+            dn = DameNLTK()
+            self.assertTrue(dn.sentence_similarity("This is a good sentence".split(), "This is a bad sentence".split()) > 0.7)
