c6ece7a8f6562c883b2374e9f63e4f9dccac6461
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-05-02 03:16:24 +0200

    regenerate-ml-json.sh: more echos explaining when a model is being created

--- src/damegender/regenerate-ml-json.sh
+++ src/damegender/regenerate-ml-json.sh
@@ -24,50 +24,86 @@ echo "--------------------------------------------------------------------------
 echo "This process is so long. You can rest for hours"
 echo "------------------------------------------------------------------------------------------"
 
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating SGD ML model                                                          "
+echo "------------------------------------------------------------------------------------------"
+
+
 python3 damegender2json.py --ml=sgd --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.sgd.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "SGD ML model is created"
+echo "           SGD ML model is created                                                        "
 echo "------------------------------------------------------------------------------------------"
+
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating SVC ML model                                                          "
+echo "------------------------------------------------------------------------------------------"
+
 
 python3 damegender2json.py --ml=svc --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.svc.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "SVC ML model is created"
+echo "           SVC ML model is created                                                        "
+echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating NLTK ML model                                                         "
 echo "------------------------------------------------------------------------------------------"
 
 python3 damegender2json.py --ml=nltk --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.nltk.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "NLTK ML model is created"
+echo "           NLTK ML model is created                                                       "
 echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           GAUSSIAN NAIVE BAYES ML model is created                                       "
+echo "------------------------------------------------------------------------------------------"
+
 
 python3 damegender2json.py --ml=gaussianNB --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.gaussianNB.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "GAUSSIAN NAIVE BAYES ML model is created"
+echo "           GAUSSIAN NAIVE BAYES ML model is created                                       "
+echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating MULTINOMIAL NAIVE BAYES ML model                                      "
 echo "------------------------------------------------------------------------------------------"
 
 python3 damegender2json.py --ml=multinomialNB --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.multinomialNB.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "MULTINOMIAL NAIVE BAYES ML model is created"
+echo "           MULTINOMIAL NAIVE BAYES ML model is created                                    "
+echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating BERNOULLI NAIVE BAYES ML model                                        "
 echo "------------------------------------------------------------------------------------------"
 
 python3 damegender2json.py --ml=bernoulliNB --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.bernoulliNB.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "BERNOULLI NAIVE BAYES ML model is created"
+echo "           BERNOULLI NAIVE BAYES ML model is created                                      "
+echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating TREE ML model                                                         "
 echo "------------------------------------------------------------------------------------------"
 
 python3 damegender2json.py --ml=tree --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.tree.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "TREE ML model is created"
+echo "           TREE ML model is created                                                       "
+echo "------------------------------------------------------------------------------------------"
+
+echo "------------------------------------------------------------------------------------------"
+echo "           Creating MULTILAYER PERCEPTRON ML model                                        "
 echo "------------------------------------------------------------------------------------------"
 
 python3 damegender2json.py --ml=mlp --csv=files/names/allnoundefined.csv --jsonoutput=files/names/allnoundefined.csv.mlp.json
 
 echo "------------------------------------------------------------------------------------------"
-echo "MULTILAYER PERCEPTRON ML model is created"
+echo "           MULTILAYER PERCEPTRON ML model is created                                      "
 echo "------------------------------------------------------------------------------------------"
