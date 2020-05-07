fc633b92396393b995161ddf5abb86232a894646
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-05-07 18:41:00 +0200

    errors.py: genderguesser is not running with jsondownloaded

--- src/damegender/errors.py
+++ src/damegender/errors.py
@@ -29,19 +29,39 @@ from app.dame_genderapi import DameGenderApi
 from app.dame_genderize import DameGenderize
 from app.dame_nameapi import DameNameapi
 from app.dame_namsor import DameNamsor
-
-import os
+import sys,os
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv', default="files/names/min.csv")
-parser.add_argument('--jsondownloaded', required=True, help="files/names/genderapifiles_names_min.csv.json")
+parser.add_argument('--csv', default='files/names/min.csv')
+parser.add_argument('--jsondownloaded', help="files/names/genderapifiles_names_min.csv.json")
+parser.add_argument('--api', default='damegender', choices=['damegender', 'namsor', 'genderize', 'genderapi', 'nameapi', 'genderguesser'])
 args = parser.parse_args()
 
 d = DameSexmachine()
-print("Damegender with %s has: " % args.csv)
-gl1 = d.gender_list(path=args.csv)
-gl2 = d.json2guess_list(jsonf=args.jsondownloaded, binary=True)        
 
+
+if (args.api in ['damegender', 'namsor', 'genderize', 'genderapi', 'nameapi']):
+    try:
+        file = open(args.jsondownloaded)
+    except FileNotFoundError:
+        print("---------------------------------------------------------------------------------------------------------------")
+        print("If you are using damegender, namsor, genderize, genderapi, or nameapi. You must introduce a json in a real path")
+        print("---------------------------------------------------------------------------------------------------------------")
+        
+elif ((args.api=='genderguesser') and (args.jsondownloaded)):
+    print("---------------------------------------------------------------------------")    
+    print("You don't need introduce jsondownloaded argument using genderguesser option")
+    print("---------------------------------------------------------------------------")    
+
+if (args.api == "genderguesser"):
+    dg = DameGenderGuesser()
+    gl2 = dg.guess_list(path=args.csv, binary=True)
+    print("Damegender with %s has: " % args.csv)
+else:
+    gl2 = d.json2guess_list(jsonf=args.jsondownloaded, binary=True)             
+    print("Gender Guesser with %s has: " % args.csv)
+
+gl1 = d.gender_list(path=args.csv)
 ec = d.error_coded(gl1, gl2)
 print("+ The error code: %s" % ec)
 ecwa = d.error_coded_without_na(gl1, gl2)
