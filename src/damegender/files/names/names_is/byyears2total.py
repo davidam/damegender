18118436ba2c4ea2790f0570d5784f0a7f47e547
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-01 05:53:36 +0200

    files/names/names_is/byyears2total.py: start script, creating json

---
+++ src/damegender/files/names/names_is/byyears2total.py
@@ -0,0 +1,300 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import csv
+import json
+import unidecode
+import unicodedata
+
+females2008 = "females2008.csv"
+females2018 = "females2018.csv"
+females2019 = "females2019.csv"
+males2008 = "males2008.csv"
+males2018 = "males2018.csv"
+males2019 = "males2019.csv"
+
+dicc = {}
+
+dicc["Julia"] = {}
+dicc["Julia"]["1954"] = {}
+dicc["Julia"]["1954"]["female"] = 1389
+dicc["Julia"]["1955"] = {}
+dicc["Julia"]["1955"]["female"] = 1384
+dicc["Julia"]["total"] = {}
+dicc["Julia"]["total"]["female"] = 11384
+print(dicc)
+
+def delete_duplicated(l):
+    if (len(l) == 0):
+        return l
+    else:
+        rest = []
+        for i in l:
+            if (i != l[0]):
+                rest = rest + [i]
+    return [l[0]] + delete_duplicated(rest)
+
+def drop_quotes(s):
+    aux = ""
+    for c in unicodedata.normalize('NFD', str(s)):
+        if ((c != '"') and (c != "'")):
+            aux = aux + c
+    return aux
+
+
+diccfemales = {}
+diccmales = {}
+l = [females2008, females2018, females2019, males2008, males2018, males2019]
+print(l)
+
+# females
+
+females = [females2008, females2018, females2019]
+
+for i in females:
+    if (i == "females2008.csv"):
+        year = "2008"
+    elif (i == "females2018.csv"):
+        year = "2018"
+    elif (i == "females2019.csv"):
+        year = "2019"
+        
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)
+        for row1 in reader1:
+            diccfemales[row1[0]] = {}
+        csvfile1.close()
+
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)    
+        for row1 in reader1:
+            diccfemales[row1[0]][year] = {}
+        csvfile1.close()
+
+for i in females:
+    if (i == "females2008.csv"):
+        year = "2008"
+    elif (i == "females2018.csv"):
+        year = "2018"
+    elif (i == "females2019.csv"):
+        year = "2019"
+    
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)    
+        for row1 in reader1:
+            diccfemales[row1[0]][year] = row1[1]
+        csvfile1.close()
+    
+# males
+
+males = [males2008, males2018, males2019]
+
+for i in males:
+    if (i == "males2008.csv"):
+        year = "2008"
+    elif (i == "males2018.csv"):
+        year = "2018"
+    elif (i == "males2019.csv"):
+        year = "2019"
+        
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)
+        for row1 in reader1:
+            diccmales[row1[0]] = {}
+        csvfile1.close()
+
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)    
+        for row1 in reader1:
+            diccmales[row1[0]][year] = {}
+        csvfile1.close()
+
+for i in males:
+    if (i == "males2008.csv"):
+        year = "2008"
+    elif (i == "males2018.csv"):
+        year = "2018"
+    elif (i == "males2019.csv"):
+        year = "2019"
+    
+    with open(i) as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+        next(reader1, None)    
+        for row1 in reader1:
+            diccmales[row1[0]][year] = row1[1]
+        csvfile1.close()
+
+print(diccmales)        
+json.dumps(diccmales)        
+fo = open("males.json", "w")
+fo.write("[" + str(diccmales) + "]")
+fo.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    # with open(pathyearsmales) as csvfile2:
+    #     reader2 = csv.reader(csvfile2, delimiter=',', quotechar='|')
+    #     next(reader2, None)
+    #     for row2 in reader2:
+    #         diccmales[drop_quotes(row2[0])][i] = drop_quotes(row2[1])
+    # csvfile2.close()
+
+    
+#     pathyearsmales = "male_cy" + str(i) + "_top.csv"
+#     pathyearsfemales = "female_cy" + str(i) + "_top.csv"
+
+#     # Se inicializan los nombres de todos los ficheros
+#     with open(pathyearsfemales) as csvfilef:
+#         reader = csv.reader(csvfilef, delimiter=',', quotechar='|')
+#         next(reader, None)
+#         l = []
+#         for row1 in reader:
+#             # this for is only to set the diccfemales
+#             diccfemales[drop_quotes(row1[0])] = {}
+#         csvfilef.close()
+
+#     with open(pathyearsmales) as csvfilem:
+#         reader = csv.reader(csvfilem, delimiter=',', quotechar='|')
+#         next(reader, None)
+#         l = []
+#         for row1 in reader:
+#             # this for is only to set the diccmales
+#             diccmales[drop_quotes(row1[0])] = {}
+#         csvfilem.close()
+
+# print("--------------------------------------------------------------------------")                
+# print("----------------------------- FEMALES ------------------------------------")        
+# print(diccfemales)
+# print(len(diccfemales))
+# print("--------------------------------------------------------------------------")
+# print("--------------------------------------------------------------------------")                
+
+
+# print("--------------------------------------------------------------------------")                
+# print("----------------------------- MALES --------------------------------------")        
+# print(diccmales)
+# print(len(diccmales))
+# print("--------------------------------------------------------------------------")
+# print("--------------------------------------------------------------------------")                
+
+
+# for i in diccmales.keys():
+#     for j in range(1944, 2013):
+#         diccmales[i][j] = {}
+
+# for i in diccfemales.keys():
+#     for j in range(1944, 2013):
+#         diccfemales[i][j] = {}
+
+        
+
+# for i in range(1944, 2013):
+
+#     pathyearsmales = "male_cy" + str(i) + "_top.csv"
+#     pathyearsfemales = "female_cy" + str(i) + "_top.csv"
+
+#     with open(pathyearsfemales) as csvfile1:
+#         reader1 = csv.reader(csvfile1, delimiter=',', quotechar='|')
+#         next(reader1, None)
+#         for row1 in reader1:
+#             diccfemales[drop_quotes(row1[0])][i] = drop_quotes(row1[1])
+#     csvfile1.close()        
+    
+#     with open(pathyearsmales) as csvfile2:
+#         reader2 = csv.reader(csvfile2, delimiter=',', quotechar='|')
+#         next(reader2, None)
+#         for row2 in reader2:
+#             diccmales[drop_quotes(row2[0])][i] = drop_quotes(row2[1])
+#     csvfile2.close()
+    
+# #print(diccmales)
+# #print(diccfemales)
+
+# for i in diccmales.keys():
+#     total = 0
+#     for j in range(1944, 2013):
+#         if (diccmales[i][j] =={}):
+#             diccmales[i][j] = 0
+
+# for i in diccfemales.keys():
+#     total = 0
+#     for j in range(1944, 2013):
+#         if (diccfemales[i][j] =={}):
+#             diccfemales[i][j] = 0
+            
+# jsonf = json.dumps(diccfemales)
+# fo = open("aufemales.json", "w")
+# fo.write(jsonf)
+# # Cerramos el archivo fichero.txt
+# fo.close()
+
+# jsonm = json.dumps(diccmales)
+# fo = open("aumales.json", "w")
+# fo.write(jsonm)
+# # Cerramos el archivo fichero.txt
+# fo.close()
+
+# string = ""
+
+# aumalesf = open("aumales.csv", "w")
+
+   
+# for i in diccmales.keys():
+#     count = 0
+#     total = 0
+#     for j in range(1944, 2013):
+#         try: 
+#             total = total + int(diccmales[i][j])
+#         except ValueError:
+#             pass
+#     string = str(i) + "," + str(total) + "\n"        
+#     aumalesf.write(string)
+#     count = count + 1
+
+# aufemalesf = open("aufemales.csv", "w")
+
+# for i in diccfemales.keys():
+#     total = 0
+#     for j in range(1944, 2013):
+#         total = total + int(diccfemales[i][j])
+#     string = str(i) + "," + str(total) + "\n"        
+#     aufemalesf.write(string)
+
+
