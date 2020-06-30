b893ed01de4289668badab733300bbab98554acc
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-30 19:20:11 +0200

    logs-count.sh: add copyright note

--- src/damegender/logs-count.sh
+++ src/damegender/logs-count.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
 python3 count-debian-gender.py > files/logs/count-debian-gender-$(date "+%Y-%m-%d").txt
 
 python3 count-gnu.py --show=all > files/logs/count-gnu-all-$(date "+%Y-%m-%d").txt
