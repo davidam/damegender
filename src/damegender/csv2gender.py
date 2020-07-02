2754974937407c7da4dc0877dc5ca546360d029b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-07-02 22:59:38 +0200

    csv2gender.py: improve csv2gender based on count python scripts

--- src/damegender/csv2gender.py
+++ src/damegender/csv2gender.py
@@ -21,20 +21,51 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-from app.dame_sexmachine import DameSexmachine
+from app.dame_gender import Gender
+from app.dame_utils import DameUtils
 import sys
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="csv file")
+parser.add_argument('--first_name_position', required=True, type=int, choices=[0, 1, 2, 3, 4], default=0)
+# parser.add_argument('--second_name_position', type=int, default=1)
+# parser.add_argument('--first_surname_position', type=int, default=2)
+# parser.add_argument('--second_surname_position', type=int, default=3)
+#parser.add_argument('--sex_position', required=True, type=int, choices=[0, 1, 2, 3, 4], default=4)
 parser.add_argument('--version', action='version', version='0.1')
 args = parser.parse_args()
-if (len(sys.argv) > 1):
-    s = DameSexmachine()
-    if ((args.path == 'files/names/partial.csv') or (args.path == 'files/names/all.csv')):
-        s.gender_list(args.path)
+
+print(args.first_name_position)
+#print(args.sex_position)
+
+du = DameUtils()
+s = Gender()
+
+males = 0
+females = 0
+unknows = 0
+
+males_list = []
+females_list = []
+unknows_list = []
+
+nameslist = du.csvcolumn2list(args.path)
+l = []
+
+for firstname in nameslist:
+    sex = s.guess(firstname, binary=False, dataset='us')
+    if (sex == "male"):
+        males_list.append(firstname)
+    elif (sex == "female"):
+        females_list.append(firstname)            
     else:
-        print("Perhaps this file has not the right format")
-    print("The number of males in %s is %s" % (str(args.path), str(s.males)))
-    print("The number of females in %s is %s" % (str(args.path), str(s.females)))
-    print("The number of gender not recognised in %s is %s" % (str(args.path), str(s.unknown)))
+        unknows_list.append(firstname)
+    l.append([firstname, sex])
+print(l)
+    
+if (len(sys.argv) > 1):
+    print("The number of males in %s is %s" % (str(args.path), str(len(males_list))))
+    print("The number of females in %s is %s" % (str(args.path), str(len(females_list))))
+    print("The number of gender not recognised in %s is %s" % (str(args.path), str(len(unknows_list))))
 
