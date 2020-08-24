7fb4b07bee95cf58e66059bd4aeeec6aa7eec1a4
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-08-24 10:37:53 +0200

    applying the feminist license

--- src/damegender/deleteapikeys.sh
+++ src/damegender/deleteapikeys.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# You can share, copy and modify this software if you are a woman or you
+# are David Arroyo Menéndez and you include this note.
+
 rm files/apikeys/*
 echo "[DEFAULT]
 genderapi = no
--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# Copyright (C) 2019  David Arroyo Menéndez
+# Copyright (C) 2020  David Arroyo Menéndez
 
 # Author: David Arroyo Menéndez <davidam@gnu.org>
 # Maintainer: David Arroyo Menéndez <davidam@gnu.org>
 
-# This file is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Damegender; see the file LICENSE.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301 USA,
+# You can share, copy and modify this software if you are a woman or you
+# are David Arroyo Menéndez and you include this note.
 
 # First, we save the current config and create the config for the tests
 
@@ -186,7 +174,7 @@ else
 	echo "infofeatures test is ok"
 fi
 
-python3 csv2gender.py files/names/partial.csv --first_name_position=0 --dataset=us --noshow > files/tests/csv2genderpartial-$(date "+%Y-%m-%d-%H").txt
+xpython3 csv2gender.py files/names/partial.csv --first_name_position=0 --dataset=us > files/tests/csv2genderpartial-$(date "+%Y-%m-%d-%H").txt
 
 if ! cmp files/tests/csv2genderpartial.txt files/tests/csv2genderpartial-$(date "+%Y-%m-%d-%H").txt
 then
