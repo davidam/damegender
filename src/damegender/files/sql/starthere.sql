ac18fef9e4d36111aa0cb834d963a1b0116c3979
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-09-15 00:38:58 +0200

    files/sql/starthere.sql: add license and fill country name

--- src/damegender/files/sql/starthere.sql
+++ src/damegender/files/sql/starthere.sql
@@ -1,30 +1,35 @@
+-- Copyright (C) 2020  David Arroyo Menéndez
+-- Author: David Arroyo Menéndez <davidam@gnu.org>
+-- Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+-- You can share, copy and modify this software if you are a woman or you
+-- are David Arroyo Menéndez and you include this note.
 
 
-drop database dame;
-create database if not exists dame;
-use dame;
+drop database damegender;
+create database if not exists damegender;
+use damegender;
 
 
 CREATE TABLE Countries (
     CountryId int NOT NULL AUTO_INCREMENT,
-    Name_en varchar(255),
     Locale varchar(255) NOT NULL,
+    Name_en varchar(255),    
     PRIMARY KEY (CountryId)
 );
 
-insert into Countries values (1, 'au');
-insert into Countries values (2, 'be');
-insert into Countries values (3, 'ca');
-insert into Countries values (4, 'es');
-insert into Countries values (5, 'fi');
-insert into Countries values (6, 'ie');
-insert into Countries values (7, 'is');
-insert into Countries values (8, 'it');
-insert into Countries values (9, 'nz');
-insert into Countries values (10, 'pt');
-insert into Countries values (11, 'uk');
-insert into Countries values (12, 'us');
-insert into Countries values (13, 'uy');
+insert into Countries values (1, 'au', 'Australia');
+insert into Countries values (2, 'be', 'Belgium');
+insert into Countries values (3, 'ca', 'Canada');
+insert into Countries values (4, 'es', 'Spain');
+insert into Countries values (5, 'fi', 'Finland');
+insert into Countries values (6, 'ie', 'Ireland');
+insert into Countries values (7, 'is', 'Island');
+insert into Countries values (8, 'it', 'Italy');
+insert into Countries values (9, 'nz', 'Norway');
+insert into Countries values (10, 'pt', 'Portugal');
+insert into Countries values (11, 'uk', 'United Kingdom');
+insert into Countries values (12, 'us', 'United States of America');
+insert into Countries values (13, 'uy', 'Uruguay');
 
 CREATE TABLE Names (
     NameId int NOT NULL AUTO_INCREMENT,
