174bf208f1ed9c50e31fb25341c813d4a248d03f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-09-20 16:48:29 +0200

    preparing files in src/damegender towards release, changing feminist license to gplv3

--- src/damegender/test/test_dame_namsor.py
+++ src/damegender/test/test_dame_namsor.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2020  David Arroyo Menéndez
+# Copyright (C) 2018  David Arroyo Menéndez
 
 # Author: David Arroyo Menéndez <davidam@gnu.org>
 # Maintainer: David Arroyo Menéndez <davidam@gnu.org>
 
-# You can share, copy and modify this software if you are a woman or you
-# are David Arroyo Menéndez and you include this note.
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 
 import unittest
 import os
