80d5afd22d0ea91dfee654e2a65baa040fcea836
David Arroyo Menéndez <davidam@gmail.com>
2020-11-10 21:53:23 +0100

    fl2gplv3.sh: add license argument

--- src/damegender/fl2gplv3.sh
+++ src/damegender/fl2gplv3.sh
@@ -19,15 +19,24 @@
 
 cp $1 new.txt
 FILE=$1
+LICENSE=$2
 if [ -f "$FILE" ]; then
     echo "File exists"
 else
     echo "Insert a file as argument, please"
 fi    
 
+if [ -f "$LICENSE" ]; then
+    echo "GPL header file exists"
+else
+    echo "Insert a GPL header file as argument, please"
+fi    
+
+
+
 sed '1,5d' new.txt > tmp.txt
-cp files/gpl.txt nuevo_fichero.txt
-cat tmp5.txt >> nuevo_fichero.txt
+cp $2 nuevo_fichero.txt
+cat tmp.txt >> nuevo_fichero.txt
 echo "Going to replace feminist license to gplv3"
 cp nuevo_fichero.txt $1
 
