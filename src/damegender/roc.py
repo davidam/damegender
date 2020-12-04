167e8ca1fe089baff1a1f5772b42331f82eb9467
David Arroyo Menéndez <davidam@gmail.com>
2020-12-04 18:40:55 +0100

    gpl to fl

--- src/damegender/roc.py
+++ src/damegender/roc.py
@@ -1,44 +1,11 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/bash
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
-# Damegender is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Damegender is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Damegender.  If not, see <https://www.gnu.org/licenses/>.
+# You can share, copy and modify this software if you are a woman or you
+# are David Arroyo Menéndez and you include this note.
 
 
-
-"""
-================================
-ROC Curve with Visualization API
-================================
-Scikit-learn defines a simple API for creating visualizations for machine
-learning. The key features of this API is to allow for quick plotting and
-visual adjustments without recalculation. In this example, we will demonstrate
-how to use the visualization API by comparing ROC curves.
-"""
-print(__doc__)
-
-##############################################################################
-# Load Data and Train a SVC
-# -------------------------
-# First, we load the wine dataset and convert it to a binary classification
-# problem. Then, we train a support vector classifier on a training dataset.
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import plot_roc_curve
-from sklearn.datasets import load_wine
-from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import GaussianNB
