ce97e6472020907589e075383da7f3c48c5c8e62
David Arroyo Menéndez <davidam@gmail.com>
2021-04-12 09:30:42 +0200

    gendergoogle.py: python header

--- src/damegender/gendergoogle.py
+++ src/damegender/gendergoogle.py
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
 # You can share, copy and modify this software if you are a woman or you
 # are David Arroyo Menéndez and you include this note.
-
-
 
 
 """Simple command-line example for Custom Search.
