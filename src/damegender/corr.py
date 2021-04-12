1398505fee93b9188cafc5b9f64a0df761428ba4
David Arroyo Menéndez <davidam@gmail.com>
2021-04-12 10:09:19 +0200

    corr.py: python header

--- src/damegender/corr.py
+++ src/damegender/corr.py
@@ -1,12 +1,11 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
 # You can share, copy and modify this software if you are a woman or you
 # are David Arroyo Menéndez and you include this note.
-
-
-
 
 import numpy as np
 import pandas as pd
