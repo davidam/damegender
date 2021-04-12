be332bd958b23868940c535999657dc082f8d9e5
David Arroyo Menéndez <davidam@gmail.com>
2021-04-12 09:27:11 +0200

    jokes.py: improvements in import

--- src/damegender/jokes.py
+++ src/damegender/jokes.py
@@ -1,16 +1,20 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
 # You can share, copy and modify this software if you are a woman or you
 # are David Arroyo Menéndez and you include this note.
 
+import re
+import argparse
+from random import randint
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--total', default="es", choices=['en', 'es'])
 args = parser.parse_args()
 
-import re
 
 filepath_es="files/jokes.es.txt"
 filepath_en="files/jokes.en.txt"
@@ -26,7 +30,7 @@ with open(filepath,'r') as f:
   for l in f:
     lines_count = lines_count +1
 
-num_sentence = random.randint(1,lines_count+1)
+num_sentence = randint(1,lines_count+1)
 
 with open(filepath) as fp:
     for cnt, line in enumerate(fp):
