fc546371c9a0eaea6bd0952c2654c11e8fe3e46a
David Arroyo Menéndez <davidam@gmail.com>
2021-04-22 12:22:13 +0200

    testsbycommandsperceval.sh: add colors

--- src/damegender/testsbycommandsperceval.sh
+++ src/damegender/testsbycommandsperceval.sh
@@ -20,15 +20,20 @@
 #  Boston, MA 02110-1301 USA,
 
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
 rm -rf /tmp/clonedir
 
 python3 git2gender.py https://github.com/davidam/orgguide-es.git --directory="/tmp/clonedir" > files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt
 
 if ! cmp files/tests/git2gender1.txt files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
-	echo "git2gender1 test is failing"
+	echo -e "git2gender1 test is ${RED}failing${NC}"
 else
-	echo "git2gender1 test is ok"
+	echo -e "git2gender1 test is ${GREEN}ok${NC}"
 fi
 
 rm -rf /tmp/clonedrupal
@@ -37,18 +42,18 @@ python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --director
 
 if ! cmp files/tests/git2genderdrupal.txt files/tests/git2genderdrupal-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
-	echo "git2genderdrupal test is failing"
+	echo -e "git2genderdrupal test is ${RED}failing${NC}"
 else
-	echo "git2genderdrupal test is ok"
+	echo -e "git2genderdrupal test is ${GREEN}ok${NC}"
 fi
 
 python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --directory="/tmp/clonedrupal" --show=all --verbose --language=es > files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt
 
 if ! cmp files/tests/git2genderverbose.txt files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
-	echo "git2genderverbose test is failing"
+	echo -e "git2genderverbose test is ${RED}failing${NC}"
 else
-	echo "git2genderverbose test is ok"
+	echo -e "git2genderverbose test is ${GREEN}ok${NC}"
 fi
 
 cd files/mbox
@@ -58,9 +63,9 @@ python3 mail2gender.py http://mail-archives.apache.org/mod_mbox/httpd-announce/ 
 
 if ! cmp files/tests/mail2gender.txt files/tests/mail2gender-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
 then
-	echo "mail2gender test is failing"
+	echo -e "mail2gender test is ${RED}failing${NC}"
 else
-	echo "mail2gender test is ok"
+	echo -e "mail2gender test is ${GREEN}ok${NC}"
 fi
 
 rm -rf /tmp/clonedir
