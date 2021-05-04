beb58d21672b5e5507387f6ddbbdf4d294b8c7be
David Arroyo Menéndez <davidam@gmail.com>
2021-05-04 06:01:59 +0200

    setup.py: change version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def drop_pwd(s):
 
 setup(name='damegender',
       python_requires='>3.6',
-      version='0.3.3.post2',
+      version='0.3.3.post4',
       description='Gender Detection Tool by David Arroyo MEnéndez',
       long_description=long_description,
       long_description_content_type='text/markdown',
