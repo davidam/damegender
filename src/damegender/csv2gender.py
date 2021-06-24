0650c56ca990b239910d520a5aa9bc8aa7f74927
David Arroyo Menéndez <davidam@gmail.com>
2021-06-24 12:42:20 +0200

    csv2gender.py: fixing PEP8

--- src/damegender/csv2gender.py
+++ src/damegender/csv2gender.py
@@ -14,17 +14,23 @@ from app.dame_gender import Gender
 from app.dame_utils import DameUtils
 import sys
 import argparse
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="csv file")
-parser.add_argument('--first_name_position', required=True, type=int, choices=[0, 1, 2, 3, 4], default=0)
-parser.add_argument('--dataset', default="us", choices=['at', 'au', 'be', 'ca', 'de', 'dk', 'es', 'fi', 'gb', 'ie', 'ine', 'inter', 'is', 'mx', 'nz', 'pt', 'si', 'uy', 'us', 'genderguesser'])
+parser.add_argument('--first_name_position', required=True,
+                    type=int, choices=[0, 1, 2, 3, 4], default=0)
+parser.add_argument('--dataset', default="us",
+                    choices=['at', 'au', 'be', 'ca', 'de', 'dk', 'es',
+                             'fi', 'gb', 'ie', 'ine', 'inter', 'is', 'mx',
+                             'nz', 'pt', 'si', 'uy', 'us', 'genderguesser'])
 parser.add_argument('--outcsv', default="files/names/out.csv")
 parser.add_argument('--outimg', default="files/images/csv2gender.png")
 parser.add_argument('--title', default="People grouped by gender")
 parser.add_argument('--noshow', dest='noshow', action='store_true')
 parser.add_argument('--skip_header', dest='skip_header', action='store_true')
-parser.add_argument('--delete_duplicated', dest='delete_duplicated', action='store_true')
+parser.add_argument('--delete_duplicated', dest='delete_duplicated',
+                    action='store_true')
 parser.add_argument('--verbose', dest='verbose', action='store_true')
 parser.add_argument('--version', action='version', version='0.3')
 args = parser.parse_args()
@@ -46,7 +52,7 @@ if (args.skip_header):
 else:
     nameslist = du.csvcolumn2list(args.path, header=False)
 
-l = []
+l1 = []
 
 for firstname in nameslist:
     firstname = du.drop_quotes(firstname)
@@ -57,10 +63,10 @@ for firstname in nameslist:
         females_list.append(firstname)
     else:
         unknows_list.append(firstname)
-    l.append([firstname, sex])
+    l1.append([firstname, sex])
 
 file = open(args.outcsv, "w")
-for i in l:
+for i in l1:
     file.write(str(i[0])+","+str(i[1]) + "\n")
 
 file.close()
@@ -71,37 +77,39 @@ if (args.delete_duplicated):
     unknows_list = du.delete_duplicated(unknows_list)
 
 if (len(sys.argv) > 1):
-    print("------------------------------------------------------------------------")
-    print("The number of males in %s is %s" % (str(args.path), str(len(males_list))))
-    if ((args.verbose) and (len(males_list) >0)):
+    print("---------------------------------------------------------------")
+    print("The number of males in %s is %s" %
+          (str(args.path), str(len(males_list))))
+    if ((args.verbose) and (len(males_list) > 0)):
         print("the males list:")
         for i in males_list:
-           print(i)
+            print(i)
 
-    print("------------------------------------------------------------------------")
-    print("The number of females in %s is %s" % (str(args.path), str(len(females_list))))
-    if ((args.verbose) and (len(females_list) >0)):
+    print("---------------------------------------------------------------")
+    print("The number of females in %s is %s" %
+          (str(args.path), str(len(females_list))))
+    if ((args.verbose) and (len(females_list) > 0)):
         print("the females list:")
         for i in females_list:
-           print(i)
+            print(i)
 
-    print("------------------------------------------------------------------------")
-    print("The number of gender not recognised in %s is %s" % (str(args.path), str(len(unknows_list))))
-    if ((args.verbose) and (len(unknows_list) >0)):
+    print("---------------------------------------------------------------")
+    print("The number of gender not recognised in %s is %s" %
+          (str(args.path), str(len(unknows_list))))
+    if ((args.verbose) and (len(unknows_list) > 0)):
         print("the unknowns list:")
         for i in unknows_list:
-           print(i)
+            print(i)
 
-
-
-import matplotlib.pyplot as plt
 
 nmales = len(males_list)
 nfemales = len(females_list)
 nunknows = len(unknows_list)
 
 data = [nmales, nfemales, nunknows]
-gender = ["Males: " + str(nmales),"Females: " + str(nfemales),"Unknows: " + str(nunknows)]
+gender = ["Males: " + str(nmales),
+          "Females: " + str(nfemales),
+          "Unknows: " + str(nunknows)]
 plt.title(args.title)
 plt.pie(data, labels=gender, autopct="%0.1f %%")
 plt.axis("equal")
