0a84a0c6fe8d80fa0db8791846cf8a67f4728742
David Arroyo Menéndez <davidam@gmail.com>
2021-07-01 16:21:41 +0200

    testmergeinterfiles.sh: replace /usr/bin/bash by /bin/bash

--- src/damegender/testmergeinterfiles.sh
+++ src/damegender/testmergeinterfiles.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 #  Copyright (C) 2021 David Arroyo Menéndez
 
@@ -19,36 +19,36 @@ python3 mergeinterfiles.py --file1="files/names/names_inter/dkfemales10.csv" --f
 # simple test adding elements and making some sum
 if ! cmp files/tests/testdkde.csv files/tests/testdkde-$(date "+%Y-%m-%d-%H").csv >/dev/null 2>&1
 then
-    echo "test is ${RED}failing${NC}"
+    echo -e "test is ${RED}failing${NC}"
 else
-    echo "test is ${GREEN}ok${NC}"
+    echo -e "test is ${GREEN}ok${NC}"
 fi
 
 python3 mergeinterfiles.py --file1="files/names/names_inter/befemales10.csv" --file2="files/names/names_inter/dkfemales10.csv" --output=files/tests/testbedk-$(date "+%Y-%m-%d-%H").csv
 # test cheking white space and upcase and downcase merging files
 if ! cmp files/tests/testbedk.csv files/tests/testbedk-$(date "+%Y-%m-%d-%H").csv >/dev/null 2>&1
 then
-    echo "testbedk is ${RED}failing${NC}"
+    echo -e "testbedk is ${RED}failing${NC}"
 else
-    echo "testbedk is ${GREEN}ok${NC}"
+    echo -e "testbedk is ${GREEN}ok${NC}"
 fi
 
 python3 mergeinterfiles.py --file1="files/names/names_inter/atfemales10.csv" --file2="files/names/names_inter/defemales10.csv" --output=files/tests/testatde-$(date "+%Y-%m-%d-%H").csv
 # test checking only upcase and downcase merging files
 if ! cmp files/tests/testatde.csv files/tests/testatde-$(date "+%Y-%m-%d-%H").csv >/dev/null 2>&1
 then
-    echo "testatde is ${RED}failing${NC}"
+    echo -e "testatde is ${RED}failing${NC}"
 else
-    echo "testatde is ${GREEN}ok${NC}"
+    echo -e "testatde is ${GREEN}ok${NC}"
 fi
 
 python3 mergeinterfiles.py --file1=files/names/names_inter/esfemeninos10.csv --file2=files/names/names_inter/ptfemales10.csv --output=files/tests/testptes-$(date "+%Y-%m-%d-%H").csv
 # checking accents
 if ! cmp files/tests/testptes.csv files/tests/testptes-$(date "+%Y-%m-%d-%H").csv >/dev/null 2>&1
 then
-    echo "testptes is ${RED}failing${NC}"
+    echo -e "testptes is ${RED}failing${NC}"
 else
-    echo "testptes is ${GREEN}ok${NC}"
+    echo -e "testptes is ${GREEN}ok${NC}"
 fi
 
 
