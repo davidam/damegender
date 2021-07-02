2ca0051299082ab84fcdc93afb67cfa8b1fc426b
David Arroyo Menéndez <davidam@gmail.com>
2021-07-02 17:09:54 +0200

    pca-features.py: fixing PEP8

--- src/damegender/pca-features.py
+++ src/damegender/pca-features.py
@@ -10,8 +10,6 @@
 # You can share, copy and modify this software if you are a woman or you
 # are David Arroyo Menéndez and you include this note.
 
-
-
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -23,62 +21,61 @@ from sklearn.decomposition import PCA
 from app.dame_sexmachine import DameSexmachine
 from app.dame_gender import Gender
 
-## PARAMETERS
+# PARAMETERS
 parser = argparse.ArgumentParser()
-parser.add_argument("--categorical", default="both", choices=['both', 'noletters', 'nocategorical', 'all'])
+parser.add_argument("--categorical", default="both",
+                    choices=['both', 'noletters', 'nocategorical', 'all'])
 parser.add_argument("--components", default=0, type=int)
 args = parser.parse_args()
 
 if (args.components > 0):
-## LOAD DATASET
+    # LOAD DATASET
     g = Gender()
     if (args.categorical == "both"):
-        g.features_list2csv(categorical="both", path="files/names/allnoundefined.csv")
+        g.features_list2csv(categorical="both",
+                            path="files/names/allnoundefined.csv")
         features = "files/features_list_no_undefined.csv"
     elif (args.categorical == "noletters"):
-        g.features_list2csv(categorical="noletters", path="files/names/allnoundefined.csv")
+        g.features_list2csv(categorical="noletters",
+                            path="files/names/allnoundefined.csv")
         features = "files/features_list_cat.csv"
     elif (args.categorical == "nocategorical"):
-        g.features_list2csv(categorical="nocategorical", path="files/names/allnoundefined.csv")
+        g.features_list2csv(categorical="nocategorical",
+                            path="files/names/allnoundefined.csv")
         features = "files/features_list_no_cat.csv"
     else:
-        g.features_list2csv(categorical="both", path="files/names/all.csv")
+        g.features_list2csv(categorical="both",
+                            path="files/names/all.csv")
         features = "files/features_list.csv"
-    ## STEP1: N COMPONENTS + 1 TARGET
+    # STEP1: N COMPONENTS + 1 TARGET
     x = pd.read_csv(features)
-    #print(x.columns)
 
     y = g.dataset2genderlist(dataset="files/names/allnoundefined.csv")
-    #print(y)
 
     # STEP2: ADDING TARGET
-    target = pd.DataFrame(data = y, columns = ['target component'])
+    target = pd.DataFrame(data=y, columns=['target component'])
     finalDf = x.join(target)
-
 
     # STEP3: NORMALIZE DATA
     from sklearn import preprocessing
-    data_scaled = pd.DataFrame(preprocessing.scale(finalDf), columns = finalDf.columns)
-
+    data1 = pd.DataFrame(preprocessing.scale(finalDf), columns=finalDf.columns)
 
     # STEP4: PCA
     pca = PCA(n_components=int(args.components))
-    pca.fit_transform(data_scaled)
+    pca.fit_transform(data1)
 
     # STEP5: Dump components relations with features:
 
     finalIndex = []
     for i in range(1, int(args.components)+1):
         finalIndex.append('PC-'+str(i))
-    #print(finalIndex)
 
-    #finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2','PC-3','PC-4','PC-5','PC-6'])
-    finalDf = pd.DataFrame(pca.components_,columns=data_scaled.columns,index = finalIndex)
+    df = pd.DataFrame(pca.components_, columns=data1.columns, index=finalIndex)
 
-    jsondata = finalDf.to_json(orient='records')
+    jsondata = df.to_json(orient='records')
 
     fo = open("files/pca.json", "w")
-    fo.write(jsondata);
+    fo.write(jsondata)
     # Close json file
     fo.close()
 
@@ -86,12 +83,14 @@ if (args.components > 0):
 
     # STEP6: Dump to html file
 
-    jh = json2html.convert(json = jsondata)
+    jh = json2html.convert(json=jsondata)
     jh = "<html><body>" + jh + "</body></html>"
     fo = open("files/pca.html", "w")
-    fo.write(jh);
+    fo.write(jh)
 
     print("The html file is created in files/pca.html")
 else:
-    print("Components must be >0, try with: $ python3 pca-features.py --categorical='noletters' --components=3")
-    print("You can use $ python3 pca-components.py to determine the number of components")
+    print("Components must be >0, try with: ")
+    print("$ python3 pca-features.py --categorical='noletters' --components=3")
+    print("You can use")
+    print("$ python3 pca-components.py to determine the number of components")
