6688024ed75fa12eaa83d47a5ab6be6a2165bf3c
David Arroyo Menéndez <davidam@gmail.com>
2021-07-03 19:11:14 +0200

    test/test_dame_statistics.py: PEP8 fixes

--- src/damegender/test/test_dame_statistics.py
+++ src/damegender/test/test_dame_statistics.py
@@ -16,19 +16,20 @@ import os
 from app.dame_gender import Gender
 from app.dame_statistics import DameStatistics
 
+
 class TddInPythonExample(unittest.TestCase):
 
     def test_dame_statistics_count_true2guess(self):
         ds = DameStatistics()
         v1 = [1, 0, 1, 1, 0, 0]
         v2 = [1, 1, 1, 0, 0, 0]
-        self.assertEqual(ds.count_true2guess(v1, v2, 0, 0), 2) # femalefemale
+        self.assertEqual(ds.count_true2guess(v1, v2, 0, 0), 2)  # femalefemale
         self.assertEqual(ds.femalefemale(v1, v2), 2)
-        self.assertEqual(ds.count_true2guess(v1, v2, 1, 1), 2) # malemale
+        self.assertEqual(ds.count_true2guess(v1, v2, 1, 1), 2)  # malemale
         self.assertEqual(ds.malemale(v1, v2), 2)
-        self.assertEqual(ds.count_true2guess(v1, v2, 0, 1), 1) # femalemale
+        self.assertEqual(ds.count_true2guess(v1, v2, 0, 1), 1)  # femalemale
         self.assertEqual(ds.femalemale(v1, v2), 1)
-        self.assertEqual(ds.count_true2guess(v1, v2, 1, 0), 1) # malefemale
+        self.assertEqual(ds.count_true2guess(v1, v2, 1, 0), 1)  # malefemale
         self.assertEqual(ds.malefemale(v1, v2), 1)
         vv1 = [1, 0, 1, 1, 1]
         vv2 = [1, 1, 1, 0]
@@ -37,10 +38,12 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(ds.count_true2guess(vv2, vv1, 1, 0), 1)  # malefemale
         vvv1 = [1, 0, 2, 2, 1]
         vvv2 = [2, 0, 1, 2, 1]
-        self.assertEqual(ds.count_true2guess(vvv2, vvv1, 2, 2), 1)  # undefinedundefined
-        self.assertEqual(ds.undefinedundefined(vvv2, vvv1), 1)
-        self.assertEqual(ds.count_true2guess(vvv2, vvv1, 2, 1), 1)  # undefinedmale
-
+        t2g = ds.count_true2guess(vvv2, vvv1, 2, 2)  # undefinedundefined
+        self.assertEqual(t2g, 1)
+        uu = ds.undefinedundefined(vvv2, vvv1)
+        self.assertEqual(uu, 1)
+        t2g = ds.count_true2guess(vvv2, vvv1, 2, 1)
+        self.assertEqual(t2g, 1)  # undefinedmale
 
     def test_dame_statistics_error_coded(self):
         ds = DameStatistics()
