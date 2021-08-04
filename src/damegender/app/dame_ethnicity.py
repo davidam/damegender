55b3e0d231b77d07d6bbd50210ca8a4c8e0db197
David Arroyo Menéndez <davidam@gmail.com>
2021-08-04 20:07:20 +0200

    app: going to feminist license

--- src/damegender/app/dame_ethnicity.py
+++ src/damegender/app/dame_ethnicity.py
@@ -4,23 +4,12 @@
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
-#  Author: David Arroyo Menéndez <davidam@gmail.com>
-#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
 
-# This file is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
+# You can share, copy and modify this software if you are a woman or you
+# are David Arroyo Menéndez and you include this note.
 
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Damegender; see the file GPL.txt.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-# Boston, MA 02110-1301 USA,
 
 import csv
 from app.dame_utils import DameUtils
