4feea0063c765b1dadba0de54b649dc75ca82719
David Arroyo Menéndez <davidam@gmail.com>
2021-08-04 20:07:20 +0200

    app: going to feminist license

--- src/damegender/app/dame_genderapi.py
+++ src/damegender/app/dame_genderapi.py
@@ -4,8 +4,8 @@
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
-#  Author: David Arroyo Menéndez <davidam@gmail.com>
-#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
 
 # You can share, copy and modify this software if you are a woman or you
 # are David Arroyo Menéndez and you include this note.
