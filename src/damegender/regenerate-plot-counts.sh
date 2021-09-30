e69fe5d8832da86f9531f2d3cefca4b4df83dd2a
David Arroyo Menéndez <davidam@gmail.com>
2021-09-30 22:30:40 +0200

    *sh: going to release from fl to gplv3

--- src/damegender/regenerate-plot-counts.sh
+++ src/damegender/regenerate-plot-counts.sh
@@ -2,12 +2,23 @@
 # Copyright (C) 2021  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
-#  Author: David Arroyo Menéndez <davidam@gmail.com>
-#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
 
-# You can share, copy and modify this software if you are a woman or you
-# are David Arroyo Menéndez and you include this note.
+# This file is Free Software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
 
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with DameGender; see the file GPL.txt.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 
 echo "Warning: you could need more than 24h for the next scripts"
--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -2,11 +2,23 @@
 # Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
 # This file is part of Damegender.
 
-#  Author: David Arroyo Menéndez <davidam@gmail.com>
-#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
 
-# You can share, copy and modify this software if you are a woman or you
-# are David Arroyo Menéndez and you include this note.
+# This file is Free Software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with DameGender; see the file GPL.txt.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 
 
@@ -512,10 +524,10 @@ else
     echo -e "csv2json test is ${RED}failing${NC}"
 fi
 
-echo "cleaning temporary files"
-rm files/tests/*$(date "+%Y")*.txt
-rm files/tmp/*
+# echo "cleaning temporary files"
+# rm files/tests/*$(date "+%Y")*.txt
+# rm -rf files/tmp/*
 
-echo "restoring the config"
-cp config.cfg.backup config.cfg
-rm config.cfg.backup
+# echo "restoring the config"
+# cp config.cfg.backup config.cfg
+# rm config.cfg.backup
