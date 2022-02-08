50c33297c484343f751dc154a54a0fc431024334
David Arroyo Menéndez <davidam@gmail.com>
2022-02-08 18:44:19 +0100

    regenerate-malefemale-files.sh: adding messages about the state of regenerating process and disable Turkey

--- src/damegender/regenerate-malefemale-files.sh
+++ src/damegender/regenerate-malefemale-files.sh
@@ -22,22 +22,39 @@
 
 echo "Building csv files with name, neutral frequencies and percentages for males and females by countries"
 python3 mergeinterfiles.py --file1="files/names/names_at/atmales.csv" --file2="files/names/names_at/atfemales.csv" --output=files/names/names_at/atall.csv --malefemale
+echo "Austria done"
 python3 mergeinterfiles.py --file1="files/names/names_au/aumales.csv" --file2="files/names/names_au/aufemales.csv" --output=files/names/names_au/auall.csv --malefemale
+echo "Australia done"
 python3 mergeinterfiles.py --file1="files/names/names_be/bemales.csv" --file2="files/names/names_be/befemales.csv" --output=files/names/names_be/beall.csv --malefemale
+echo "Belgium done"
 python3 mergeinterfiles.py --file1="files/names/names_ca/camales.csv" --file2="files/names/names_ca/cafemales.csv" --output=files/names/names_ca/caall.csv --malefemale
+echo "Canada done"
 python3 mergeinterfiles.py --file1="files/names/names_de/demales.csv" --file2="files/names/names_de/defemales.csv" --output=files/names/names_de/deall.csv --malefemale
+echo "Germany done"
 python3 mergeinterfiles.py --file1="files/names/names_dk/males.csv" --file2="files/names/names_dk/females.csv" --output=files/names/names_dk/dkall.csv --malefemale
+echo "Denkmark done"
 python3 mergeinterfiles.py --file1="files/names/names_es/esmasculinos.csv" --file2="files/names/names_es/esfemeninos.csv" --output=files/names/names_es/esall.csv --malefemale
+echo "Spanish done"
 python3 mergeinterfiles.py --file1="files/names/names_fi/fimales.csv" --file2="files/names/names_fi/fifemales.csv" --output=files/names/names_fi/fiall.csv --malefemale
+echo "Finland done"
 python3 mergeinterfiles.py --file1="files/names/names_fr/frmales.csv" --file2="files/names/names_fr/frfemales.csv" --output=files/names/names_fr/frall.csv --malefemale
+echo "France done"
 python3 mergeinterfiles.py --file1="files/names/names_ie/iemales.csv" --file2="files/names/names_ie/iefemales.csv" --output=files/names/names_ie/ieall.csv --malefemale
+echo "Ireland done"
 python3 mergeinterfiles.py --file1="files/names/names_inter/intermales.csv" --file2="files/names/names_inter/interfemales.csv" --output=files/names/names_inter/interall.csv --malefemale
+echo "International done"
 python3 mergeinterfiles.py --file1="files/names/names_is/ismales.csv" --file2="files/names/names_is/isfemales.csv" --output=files/names/names_is/isall.csv --malefemale
+echo "Iceland done"
 python3 mergeinterfiles.py --file1="files/names/names_mx/hombres.csv" --file2="files/names/names_mx/mujeres.csv" --output=files/names/names_mx/mxall.csv --malefemale
+echo "Mexico done"
 python3 mergeinterfiles.py --file1="files/names/names_no/nomales.csv" --file2="files/names/names_no/nofemales.csv" --output=files/names/names_no/noall.csv --malefemale
+echo "Norway done"
 python3 mergeinterfiles.py --file1="files/names/names_nz/nzmales.csv" --file2="files/names/names_nz/nzfemales.csv" --output=files/names/names_nz/nzall.csv --malefemale
+echo "New Zealand done"
 python3 mergeinterfiles.py --file1="files/names/names_pt/ptmales.csv" --file2="files/names/names_pt/ptfemales.csv" --output=files/names/names_pt/ptall.csv --malefemale
+echo "Portugal done"
 python3 mergeinterfiles.py --file1="files/names/names_se/semales.csv" --file2="files/names/names_se/sefemales.csv" --output=files/names/names_se/seall.csv --malefemale
-python3 mergeinterfiles.py --file1="files/names/names_tr/trmales.csv" --file2="files/names/names_tr/trfemales.csv" --output=files/names/names_tr/trall.csv --malefemale
+echo "Sweden done"
+# python3 mergeinterfiles.py --file1="files/names/names_tr/trmales.csv" --file2="files/names/names_tr/trfemales.csv" --output=files/names/names_tr/trall.csv --malefemale
 python3 mergeinterfiles.py --file1="files/names/names_uy/uymasculinos.csv" --file2="files/names/names_uy/uyfemeninos.csv" --output=files/names/names_uy/uyall.csv --malefemale
-
+echo "Uruguay done"
