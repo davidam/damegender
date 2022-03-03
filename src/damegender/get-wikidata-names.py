c4cb81694fe1f3b8de306e4fe16c0b1731631e93
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-03-03 01:22:02 +0100

    get-wikidata-names.py: creation

---
+++ src/damegender/get-wikidata-names.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2022  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Damegender; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('gender', default="female",
+                    choices=['female', 'male'])
+parser.add_argument('--total', default="inter",
+                    choices=['at', 'au', 'be', 'ca', 'ch',
+                             'cn', 'de', 'dk', 'es', 'fi',
+                             'fr', 'gb', 'ie', 'is', 'no', 'nz',
+                             'mx', 'pt', 'ru', 'se', 'si',
+                             'uy', 'us'])
+args = parser.parse_args()
+
+url = 'https://query.wikidata.org/sparql'
+
+male = False
+female = False
+if (args.gender == "female"):
+    female = True
+elif (args.gender == "male"):
+    male = True
+
+if female:
+    gender = "    ?person wdt:P21 wd:Q6581072 . "
+elif male:
+    gender = "    ?person wdt:P21 wd:Q6581097 . "
+
+
+if (args.total == "at"):
+    string = "Q40"
+elif (args.total == "au"):
+    string = "Q408"
+elif (args.total == "be"):
+    string = "Q31"
+elif (args.total == "ca"):
+    string = "Q16"
+elif (args.total == "ch"):
+    string = "Q39"
+elif (args.total == "cn"):
+    string = "Q148"
+elif (args.total == "de"):
+    string = "Q183"
+elif (args.total == "dk"):
+    string = "Q35"
+elif (args.total == "es"):
+    string = "Q29"
+elif (args.total == "es"):
+    string = "Q29"
+elif (args.total == "fi"):
+    string = "Q33"
+elif (args.total == "fr"):
+    string = "Q142"
+elif (args.total == "gb"):
+    string = "Q145"
+elif (args.total == "ie"):
+    string = "Q27"
+elif (args.total == "is"):
+    string = "Q189"
+elif (args.total == "no"):
+    string = "Q20"
+elif (args.total == "nz"):
+    string = "Q664"
+elif (args.total == "pt"):
+    string = "Q45"
+elif (args.total == "ru"):
+    string = "Q159"
+elif (args.total == "se"):
+    string = "Q34"
+elif (args.total == "si"):
+    string = "Q215"
+elif (args.total == "uy"):
+    string = "Q77"
+
+    
+query2 = """
+SELECT ?name ?nameLabel ?count
+WITH {
+  SELECT ?name (count(?person) AS ?count) WHERE {
+    ?person wdt:P735 ?name . 
+""" + gender + """
+    ?person wdt:P27 wd:""" + string + """ .
+  }
+  GROUP BY ?name
+  ORDER BY DESC(?count)
+  LIMIT 100
+} AS %results
+WHERE {
+  INCLUDE %results
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+ORDER BY DESC(?count)
+"""
+r = requests.get(url, params = {'format': 'json', 'query': query2})
+data = r.json()
+
+# print(data["results"]["bindings"])
+
+print("Dumping to names.csv")
+fo = open("names.csv", "w")
+for d in data["results"]["bindings"]:
+    # print(d)
+    # print(d["count"]["value"])
+    # print(d["nameLabel"]["value"])
+    fo.write(d['nameLabel']['value'] + "," + d['count']['value'] +  "\n")
+
+
+
+
+
+# Cerramos el archivo fichero.txt
+fo.close()
