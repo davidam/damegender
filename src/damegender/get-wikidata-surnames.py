cff7f0c253ec6b6c96e11da83aa3891e7de49368
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-03-03 01:11:04 +0100

    get-wikidata-surnames.py: remove buggy inter option

--- src/damegender/get-wikidata-surnames.py
+++ src/damegender/get-wikidata-surnames.py
@@ -27,90 +27,65 @@ import argparse
 
 parser = argparse.ArgumentParser()
 # parser.add_argument("name", help="display the gender")
-parser.add_argument('total', default="inter",
+parser.add_argument('total', default="es",
                     choices=['at', 'au', 'be', 'ca', 'ch',
                              'cn', 'de', 'dk', 'es', 'fi',
                              'fr', 'gb', 'ie', 'is', 'no', 'nz',
                              'mx', 'pt', 'ru', 'se', 'si',
-                             'uy', 'us', 'inter'])
+                             'uy', 'us'])
 # More about iso codes on https://www.iso.org/obp/ui/
 parser.add_argument('--version', action='version', version='0.4')
 # parser.add_argument('--verbose', default=False, action="store_true")
 args = parser.parse_args()
 
-if (args.total == "inter"):
-    query = """
-PREFIX bd: <http://www.bigdata.com/rdf#>
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-PREFIX wikibase: <http://wikiba.se/ontology#>
-#added before 2016-10
 
-#defaultView:BubbleChart
-SELECT ?surname ?surnameLabel ?count
-WHERE
-{
-  {
-    SELECT ?surname (COUNT(?person) AS ?count) WHERE {
-      ?person (wdt:P31/wdt:P279*) wd:Q95074.
-      ?person wdt:P734 ?surname.
-    }
-    GROUP BY ?surname
-  }
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-}
-ORDER BY DESC(?count)
+if (args.total == "at"):
+    string = "Q40"
+elif (args.total == "au"):
+    string = "Q408"
+elif (args.total == "be"):
+    string = "Q31"
+elif (args.total == "ca"):
+    string = "Q16"
+elif (args.total == "ch"):
+    string = "Q39"
+elif (args.total == "cn"):
+    string = "Q148"
+elif (args.total == "de"):
+    string = "Q183"
+elif (args.total == "dk"):
+    string = "Q35"
+elif (args.total == "es"):
+    string = "Q29"
+elif (args.total == "es"):
+    string = "Q29"
+elif (args.total == "fi"):
+    string = "Q33"
+elif (args.total == "fr"):
+    string = "Q142"
+elif (args.total == "gb"):
+    string = "Q145"
+elif (args.total == "ie"):
+    string = "Q27"
+elif (args.total == "is"):
+    string = "Q189"
+elif (args.total == "no"):
+    string = "Q20"
+elif (args.total == "nz"):
+    string = "Q664"
+elif (args.total == "pt"):
+    string = "Q45"
+elif (args.total == "ru"):
+    string = "Q159"
+elif (args.total == "se"):
+    string = "Q34"
+elif (args.total == "si"):
+    string = "Q215"
+elif (args.total == "uy"):
+    string = "Q77"
 
-LIMIT"""
-
-else:
-    if (args.total == "at"):
-        string = "Q40"
-    elif (args.total == "au"):
-        string = "Q408"
-    elif (args.total == "be"):
-        string = "Q31"
-    elif (args.total == "ca"):
-        string = "Q16"
-    elif (args.total == "ch"):
-        string = "Q39"
-    elif (args.total == "cn"):
-        string = "Q148"
-    elif (args.total == "de"):
-        string = "Q183"
-    elif (args.total == "dk"):
-        string = "Q35"
-    elif (args.total == "es"):
-        string = "Q29"
-    elif (args.total == "es"):
-        string = "Q29"
-    elif (args.total == "fi"):
-        string = "Q33"
-    elif (args.total == "fr"):
-        string = "Q142"
-    elif (args.total == "gb"):
-        string = "Q145"
-    elif (args.total == "ie"):
-        string = "Q27"
-    elif (args.total == "is"):
-        string = "Q189"
-    elif (args.total == "no"):
-        string = "Q20"
-    elif (args.total == "nz"):
-        string = "Q664"
-    elif (args.total == "pt"):
-        string = "Q45"
-    elif (args.total == "ru"):
-        string = "Q159"
-    elif (args.total == "se"):
-        string = "Q34"
-    elif (args.total == "si"):
-        string = "Q215"
-    elif (args.total == "uy"):
-        string = "Q77"
-
-    url = 'https://query.wikidata.org/sparql'
-    query = """
+url = 'https://query.wikidata.org/sparql'
+query = """
 SELECT ?name ?nameLabel ?count
 WITH {
   SELECT ?name (count(?person) AS ?count) WHERE {
