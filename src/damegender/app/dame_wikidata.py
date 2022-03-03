1e9d95d8210f57bea062fd51ab516cc13db1f948
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-03-03 22:52:41 +0100

    app/dame_wikidata.py: creation

---
+++ src/damegender/app/dame_wikidata.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
+# This file is part of Damegender.
+
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with DameGender; see the file GPL.txt.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+
+class DameWikidata():
+
+    def dicc_countries(self):
+        path = {"ad": "Q228",
+                "af": "Q889",
+                "al": "Q222",
+                "at": "Q40",
+                "au": "Q408",
+                "be": "Q31",
+                "ca": "Q16",
+                "ch": "Q39",
+                "cn": "Q148",
+                "de": "Q183",
+                "dk": "Q35",
+                "dz": "Q262",                
+                "es": "Q29",
+                "fi": "Q33",
+                "fr": "Q142",
+                "gb": "Q145",
+                "ie": "Q27",
+                "is": "Q189",
+                "mx": "Q96",
+                "no": "Q20",
+                "nz": "Q664",
+                "pt": "Q45",
+                "ru": "Q159",
+                "se": "Q34",
+                "si": "Q215",
+                "us": "Q35657",
+                "uy": "Q77"}
+        return path
