76b10594557771d8a29ee7c0c0c407503114da7f
David Arroyo Menéndez <davidam@gmail.com>
2022-03-05 15:06:03 +0100

    damegender2json.py: remove not used library dame_customsearch

--- src/damegender/damegender2json.py
+++ src/damegender/damegender2json.py
@@ -34,7 +34,6 @@ from app.dame_genderguesser import DameGenderGuesser
 from app.dame_genderapi import DameGenderApi
 from app.dame_genderize import DameGenderize
 from app.dame_nameapi import DameNameapi
-from app.dame_customsearch import DameCustomsearch
 import os
 import argparse
 
