02f786dfabba5fd856e220c286e79f7c3efd1a77
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-03-17 15:59:32 +0100

    csv2gender.py: adding russian locales

--- src/damegender/csv2gender.py
+++ src/damegender/csv2gender.py
@@ -43,7 +43,8 @@ parser.add_argument('--dataset', default="us",
                     choices=['at', 'au', 'be', 'ca', 'ch', 'cn', 'de',
                              'dk', 'es', 'fi', 'fr', 'gb', 'ie', 'ine',
                              'inter', 'is', 'mx', 'no', 'nz', 'pt',
-                             'se', 'si', 'uy', 'us', 'genderguesser'])
+                             'ru', 'ru_en', 'ru_ru', 'se', 'si', 'uy',
+                             'us', 'genderguesser'])
 parser.add_argument('--outcsv', default="files/names/out.csv")
 parser.add_argument('--outjson', default="files/names/out.json")
 parser.add_argument('--outimg', default="files/images/csv2gender.png")
