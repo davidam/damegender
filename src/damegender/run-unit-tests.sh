3370c20f712950483195e620414754912ebce3d4
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-04-08 16:56:54 +0200

    run-unit-tests.sh: creation

--- src/damegender/run-unit-tests.sh
+++ src/damegender/run-unit-tests.sh
@@ -20,6 +20,46 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-nosetests3 test
+echo "Do you prefer nose or pytest? (nose|pytest)"
+
+read testing
+
+if [[ "nose" == "$testing" ]]; then
+    echo "Launching test using nose unit testing system"
+    echo "Write nose command (ex: nosetests)"
+    read nose
+    $nose test/test_dame_environment.py
+    $nose test/test_dame_ethnicity.py
+    $nose test/test_dame_gender.py
+    $nose test/test_dame_genderapi.py
+    $nose test/test_dame_genderguesser.py
+    $nose test/test_dame_genderize.py
+    $nose test/test_dame_nameapi.py
+    $nose test/test_dame_namsor.py
+    $nose test/test_dame_nltk.py
+    $nose test/test_dame_perceval.py
+    $nose test/test_dame_sexmachine.py
+    $nose test/test_dame_statistics.py
+    $nose test/test_dame_utils.py
+fi
+
+if [[ "pytest" == "$testing" ]]; then
+    echo "Launching test using pytest unit testing system"
+    echo "Write pytest command (ex: pytest)"
+    read PYTEST
+    $PYTEST test/test_dame_environment.py
+    $PYTEST test/test_dame_ethnicity.py
+    $PYTEST test/test_dame_gender.py
+    $PYTEST test/test_dame_genderapi.py
+    $PYTEST test/test_dame_genderguesser.py
+    $PYTEST test/test_dame_genderize.py
+    $PYTEST test/test_dame_nameapi.py
+    $PYTEST test/test_dame_namsor.py
+    $PYTEST test/test_dame_nltk.py
+    $PYTEST test/test_dame_perceval.py
+    $PYTEST test/test_dame_sexmachine.py
+    $PYTEST test/test_dame_statistics.py
+    $PYTEST test/test_dame_utils.py
+fi
 
 
