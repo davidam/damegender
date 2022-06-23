c4e163b0fee86e8989722b481ed2070400e6a38e
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-06-22 22:50:25 -0300

    orig2ar.py: more fixes

--- src/damegender/orig2ar.py
+++ src/damegender/orig2ar.py
@@ -9,15 +9,15 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 3, or (at your option)
 #  any later version.
-# 
+#
 #  This file is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-# 
+#
 #  You should have received a copy of the GNU General Public License
 #  along with Damegender; see the file GPL.txt.  If not, write to
-#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301 USA,
 
 import csv
@@ -27,50 +27,43 @@ du = DameUtils()
 
 country = "ar"
 outpath = "files/names/names_" + country + "/"
-outmales = outpath + country  + "males.csv"
-outfemales = outpath + country  + "females.csv"
+outmales = outpath + country + "males.csv"
+outfemales = outpath + country + "females.csv"
 
 origpath = outpath + "orig/"
 origfile = origpath + "personas.csv"
 
 with open(origfile) as csvfile:
-#    sreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-    sreader2 = csv.reader(csvfile, delimiter=';', quotechar='|')    
-    
+    sreader2 = csv.reader(csvfile, delimiter=';', quotechar='|')
     diccfemales = {}
     diccmales = {}
-    
-    # for row in sreader:
-    #     try:
-    #         if (row[3] == '1'):
-    #             diccfemales[row[1]] = {}
-    #         elif (row[3] == '2'):
-    #             diccmales[row[1]] = {}
-    #     except IndexError:
-    #         print("The program has troubles with the array indexes")
-    
     for row in sreader2:
         try:
-            if (row[3] == '1'):
-                if row[1] in diccfemales.keys():
-                    diccfemales[row[1]] = int(diccfemales[row[1]]) + 1
-                else:
-                    diccfemales[row[1]] = 1
-            elif (row[3] == '2'):
-                if row[1] in diccmales.keys():
-                    diccmales[row[1]] = int(diccmales[row[1]]) + 1
-                else:
-                    diccmales[row[1]] = 1
+            gender = str(row[3])
+            name = str(row[1])
         except IndexError:
             print("The program has troubles with the array indexes")
-
+        if (gender == '1'):
+            if name in diccfemales.keys():
+                diccfemales[name] = int(diccfemales[name]) + 1
+            else:
+                diccfemales[name] = 1
+        elif (gender == '2'):
+            if name in diccmales.keys():
+                diccmales[name] = int(diccmales[name]) + 1
+            else:
+                diccmales[name] = 1
     filefem = open(outfemales, 'w')
     filemal = open(outmales, 'w')
-            
+
     for i in diccfemales.keys():
-        filefem.write(str(i) + "," + str(diccfemales[i]) + "\n")
+        str1 = str(i) + ","
+        str1 = str1 + str(diccfemales[i]) + "\n"
+        filefem.write(str1)
     for j in diccmales.keys():
-        filemal.write(str(j) + "," + str(diccmales[j]) + "\n")        
+        str2 = str(j) + ","
+        str2 = str2 + str(diccmales[j]) + "\n"
+        filemal.write(str2)
 
 filefem.close()
 filemal.close()
