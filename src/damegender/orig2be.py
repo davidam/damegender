61bde83507a7e99085ebacda93879549553ac24d
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-07-05 13:19:21 -0300

    orig2be.py: dicc2 is for females

--- src/damegender/orig2be.py
+++ src/damegender/orig2be.py
@@ -23,7 +23,7 @@ import csv
 total = []
 
 dicc = {}
-
+dicc2 = {}
 path = "files/names/names_be/"
 origpath = "files/names/names_be/orig/"
 
@@ -168,22 +168,22 @@ with open(origpath + 'TA_POP_2009_F.txt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
         #print(','.join(row))
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 with open(origpath + 'TA_POP_2010_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
         #print(','.join(row))
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2011_F.txt') as csvfile:
@@ -191,11 +191,11 @@ with open(origpath + 'TA_POP_2011_F.txt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
         #print(','.join(row))
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2012_F.txt') as csvfile:
@@ -203,11 +203,11 @@ with open(origpath + 'TA_POP_2012_F.txt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
         #print(','.join(row))
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2013_F.txt') as csvfile:
@@ -215,79 +215,79 @@ with open(origpath + 'TA_POP_2013_F.txt') as csvfile:
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
         #print(','.join(row))
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2014_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2015_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2016_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 with open(origpath + 'TA_POP_2017_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 with open(origpath + 'TA_POP_2018_F.txt') as csvfile:
 
     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
     for row in spamreader:
-        if (row[3] in dicc.keys()):
-            val = dicc[row[3]]
-            dicc[row[3]] = int(val) + int(row[4])
+        if (row[3] in dicc2.keys()):
+            val = dicc2[row[3]]
+            dicc2[row[3]] = int(val) + int(row[4])
         else:
-            dicc[row[3]] = row[4]
+            dicc2[row[3]] = row[4]
 
 
 # with open(origpath + 'TA_POP_FEMALE_2019.txt') as csvfile:
 # 
 #     spamreader = csv.reader(csvfile, delimiter='|', quotechar='|')
 #     for row in spamreader:
-#         if (row[3] in dicc.keys()):
-#             val = dicc[row[3]]
-#             dicc[row[3]] = int(val) + int(row[4])
+#         if (row[3] in dicc2.keys()):
+#             val = dicc2[row[3]]
+#             dicc2[row[3]] = int(val) + int(row[4])
 #         else:
-#             dicc[row[3]] = row[4]
+#             dicc2[row[3]] = row[4]
 
 fo = open(path + "befemales.csv", "w")
-for i in dicc.keys():
-    fo.write(str(i) + "," + str(dicc[i]) + "\n")
+for i in dicc2.keys():
+    fo.write(str(i) + "," + str(dicc2[i]) + "\n")
 
 fo.close()
