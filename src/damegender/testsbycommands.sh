fb47fa36aff1d304f62a27b66950b795f04cf6b6
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-08-31 22:21:28 -0300

    testsbycommands.sh: adding new test

--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -531,6 +531,16 @@ else
 	echo -e  "percentage2names test is ${GREEN}ok${NC}"
 fi
 
+python3 percentage2names.py 40 --percentage_until=70 --outcsv=files/tests/40-70-$(date "+%Y-%m-%d").txt
+
+if ! diff files/tests/40-70.txt files/tests/40-70-$(date "+%Y-%m-%d").txt >/dev/null 2>&1
+then
+	echo -e  "percentage2names test is ${RED}failing${NC}"
+else
+	echo -e  "percentage2names test is ${GREEN}ok${NC}"
+fi
+
+
 
 echo "cleaning temporary files"
 rm files/tests/*$(date "+%Y")*.txt
