2c2af04d1e0624348d276c228e0d81fd51e89594
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-09-27 13:54:59 -0300

    files/names/names_tests/download.sh: we are allowing to download lucia and helena dataset, but not the redistribution

--- src/damegender/files/names/names_tests/download.sh
+++ src/damegender/files/names/names_tests/download.sh
@@ -46,3 +46,7 @@ wget -c https://osf.io/wjv7q/
 
 # FIFA
 wget -c https://www.kaggle.com/datasets/thec03u5/fifa-18-demo-player-dataset
+
+# Lucia and Helena Dataset
+wget -c https://raw.githubusercontent.com/GenderGapSTEM-PublicationAnalysis/name_gender_inference/main/name_gender_inference/test_data/raw_data/all.csv
+grep -v ',"u",' all.csv > allnoundefined.csv
