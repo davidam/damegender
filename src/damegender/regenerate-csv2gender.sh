c9f89772222395599c6381570e527e8f2417e630
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-10-13 14:29:40 -0300

    regenerate-csv2gender.sh: adding more csv2gender executions

--- src/damegender/regenerate-csv2gender.sh
+++ src/damegender/regenerate-csv2gender.sh
@@ -20,7 +20,16 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-python3 csv2gender.py files/names/names_tests/autores.csv --first_name_position=2 --dataset=inter --noshow --outcsv=files/names/names_tests/autores.interguessed.csv
+python3 csv2gender.py files/names/names_tests/autores.csv --first_name_position=2 --dataset=inter --noshow --outcsv=files/names/names_tests/autores.interguessed.csv --skip_header
 
-python3 csv2gender.py files/names/names_tests/autores.csv --first_name_position=2 --dataset=es --noshow --outcsv=files/names/names_tests/autores.esguessed.csv
+python3 csv2gender.py files/names/names_tests/autores.csv --first_name_position=2 --dataset=es --noshow --outcsv=files/names/names_tests/autores.esguessed.csv --skip_header
 
+python3 csv2gender.py files/names/names_tests/autores.csv --first_name_position=2 --dataset=us --noshow --outcsv=files/names/names_tests/autores.usguessed.csv --skip_header
+
+python3 csv2gender.py files/names/names_tests/liste_des_prenoms.csv --first_name_position=3 --dataset=inter --noshow --outcsv=files/names/names_tests/liste_des_prenoms.interguessed.csv --skip_header --delimiter_csv=';'
+
+python3 csv2gender.py files/names/names_tests/liste_des_prenoms.csv --first_name_position=3 --dataset=us --noshow --outcsv=files/names/names_tests/liste_des_prenoms.usguessed.csv --skip_header --delimiter_csv=';'
+
+python3 csv2gender.py files/names/names_tests/liste_des_prenoms.csv --first_name_position=3 --dataset=fr --noshow --outcsv=files/names/names_tests/liste_des_prenoms.frguessed.csv --skip_header --delimiter_csv=';'
+
+python3 csv2gender.py files/names/names_tests/liste_des_prenoms.csv --first_name_position=3 --dataset=es --noshow --outcsv=files/names/names_tests/liste_des_prenoms.esguessed.csv --skip_header --delimiter_csv=';'
