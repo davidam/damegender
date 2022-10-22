c225fd3af89d15ba93469cd171f4452d2c7d1491
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-10-22 08:06:07 +0200

    files/names/names_inter/remove-initials.sh: creation

---
+++ src/damegender/files/names/names_inter/remove-initials.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+#  Copyright (C) 2022 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com> 
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com> 
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+# 
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with Damegender; see the file GPL.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+#  Boston, MA 02110-1301 USA,
+
+sed '/^A\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^B\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^C\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^D\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^E\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^F\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^G\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^H\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^I\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^J\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^K\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^L\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^M\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^N\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^O\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^P\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^Q\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^R\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^S\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^T\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^U\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^V\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^W\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^X\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^Y\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+sed '/^Z\./d' interfemales.csv > tmp.txt
+cp tmp.txt interfemales.csv
+
+
+sed '/^A\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^B\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^C\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^D\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^E\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^F\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^G\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^H\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^I\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^J\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^K\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^L\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^M\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^N\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^O\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^P\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^Q\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^R\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^S\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^T\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^U\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^V\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^W\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^X\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^Y\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
+sed '/^Z\./d' intermales.csv > tmp.txt
+cp tmp.txt intermales.csv
