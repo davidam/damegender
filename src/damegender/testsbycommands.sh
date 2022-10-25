65ebb83acf189e2b73bbf21e4bc3f56343cd894f
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-10-25 07:06:43 +0200

    testsbycommands.sh: adding fifa test, accuracy test using 2 csv files

--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -397,6 +397,16 @@ else
 	echo -e  "accuracy genderize recall jsondonwloaded test is ${GREEN}ok${NC}"
 fi
 
+
+python3 accuracy.py --measure=accuracy --dataset_guess=files/names/names_tests/fifa.interguessed.csv --dataset_guess_row_name=0 --dataset_guess_row_gender=1 --dataset_test=files/names/names_tests/fifa.interguessed.csv --dataset_test_row_name=0 --dataset_test_row_gender=1 --api=damegender --dataset_test_row_gender_chars="female,male" > files/tests/accuracyfifafifa-$(date "+%Y-%m-%d").txt
+if ! diff files/tests/accuracyfifafifa.txt files/tests/accuracyfifafifa-$(date "+%Y-%m-%d").txt
+then
+	echo -e  "accuracy fifa test is ${RED}failing${NC}"
+else
+	echo -e  "accuracy fifa test is ${GREEN}ok${NC}"
+fi
+
+
 python3 damegender2json.py --notoutput --csv=files/names/min.csv --jsonoutput=files/names/min.csv.$(date "+%Y-%m-%d").json
 
 if ! diff files/names/min.csv.json files/names/min.csv.$(date "+%Y-%m-%d").json
