de5b03a829166125751964b553915b774ff370e8
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-11-18 20:11:57 +0100

    testsbycommands.sh: JMLA test fixed

--- src/damegender/testsbycommands.sh
+++ src/damegender/testsbycommands.sh
@@ -348,13 +348,13 @@ else
 	echo -e  "confusion nameapi jsondonwloaded test is ${GREEN}ok${NC}"
 fi
 
-# python3 accuracy.py --api=damegender --measure=accuracy --dataset_guess=files/names/names_tests/gender_JMLA.min.interguessed.csv --dataset_guess_row_name=0 --dataset_guess_row_gender=1 --dataset_test=files/names/names_tests/gender_JMLA.min.dta.csv --dataset_test_row_name=1 --dataset_test_row_gender=2 --dataset_test_row_gender_chars="female,male" > files/tests/accuracygender_JMLA-$(date "+%Y-%m-%d").txt
-# if ! diff files/tests/accuracygender_JMLA.txt files/tests/accuracygender_JMLA-$(date "+%Y-%m-%d").txt >/dev/null 2>&1
-# then
-# 	echo -e  "accuracy JMLA dataset test is ${RED}failing${NC}"
-# else
-# 	echo -e  "accuracy JMLA dataset test is ${GREEN}ok${NC}"
-# fi
+python3 accuracy.py --api=damegender --measure=accuracy --dataset_guess=files/names/names_tests/gender_JMLA.min.interguessed.csv --dataset_guess_position_gender=1 --dataset_test=files/names/names_tests/gender_JMLA.min.dta.csv --dataset_test_position_gender=2 --dataset_test_gender_chars="female,male" > files/tests/accuracygender_JMLA-$(date "+%Y-%m-%d").txt
+if ! diff files/tests/accuracygender_JMLA.txt files/tests/accuracygender_JMLA-$(date "+%Y-%m-%d").txt >/dev/null 2>&1
+then
+	echo -e  "accuracy JMLA dataset test is ${RED}failing${NC}"
+else
+	echo -e  "accuracy JMLA dataset test is ${GREEN}ok${NC}"
+fi
 
 # python3 accuracy.py --dataset_guess="files/names/min.csv" --api=genderapi --dataset_test="files/names/genderapifiles_names_min.csv.json" > files/tests/accuracygenderapijsondownloaded-$(date "+%Y-%m-%d").txt
 # if ! diff files/tests/accuracygenderapijsondownloaded.txt files/tests/accuracygenderapijsondownloaded-$(date "+%Y-%m-%d").txt >/dev/null 2>&1
