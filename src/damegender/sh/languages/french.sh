b69853d8d1d6fff3ca3f69c861ba13f954d45fc8
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-12-26 14:38:51 +0100

    sh/languages/french.sh: adding Burkina Faso

--- src/damegender/sh/languages/french.sh
+++ src/damegender/sh/languages/french.sh
@@ -27,14 +27,22 @@ echo "Building frenchfemales.csv"
 echo "Merging french countries, building females"
 python3 mergeinterfiles.py --file1="files/names/names_fr/frfemales.csv" --file2="files/names/names_ca/cafemales.csv" --output=files/tmp/frcafemales.csv
 python3 mergeinterfiles.py --file1="files/tmp/frcafemales.csv" --file2="files/names/names_tg/tgfemales.csv" --output=files/tmp/frcatgfemales.csv
-cp files/tmp/frcatgfemales.csv files/names/languages/frenchfemales.csv
+python3 mergeinterfiles.py --file1="files/tmp/frcafemales.csv" --file2="files/names/names_bf/bffemales.csv" --output=files/tmp/frcatgbffemales.csv
+cp files/tmp/frcatgbffemales.csv files/names/languages/frenchfemales.csv
 
 echo "Building frenchmales.csv"
 echo "Merging french countries, building males"
 python3 mergeinterfiles.py --file1="files/names/names_fr/frmales.csv" --file2="files/names/names_ca/camales.csv" --output=files/tmp/frcamales.csv
 python3 mergeinterfiles.py --file1="files/tmp/frcamales.csv" --file2="files/names/names_tg/tgmales.csv" --output=files/tmp/frcatgmales.csv
-cp files/tmp/frcatgmales.csv files/names/languages/frenchmales.csv
+python3 mergeinterfiles.py --file1="files/tmp/frcatgfemales.csv" --file2="files/names/names_bf/bfmales.csv" --output=files/tmp/frcatgbfmales.csv
+cp files/tmp/frcatgbfmales.csv files/names/languages/frenchmales.csv
 
+#echo "Building frenchsurnames.csv"
+#echo "Merging french countries, building surnames"
+#python3 mergeinterfiles.py --file1="files/names/names_fr/frsurnames.csv" --file2="files/names/names_ca/casurnames.csv" --output=files/tmp/frcasurnames.csv
+#python3 mergeinterfiles.py --file1="files/tmp/frcasurnames.csv" --file2="files/names/names_tg/tgsurnames.csv" --output=files/tmp/frcatgsurnames.csv
+#python3 mergeinterfiles.py --file1="files/tmp/frcatgsurnames.csv" --file2="files/names/names_bf/bfsurnames.csv" --output=files/tmp/frcatgbfsurnames.csv
+#cp files/tmp/frcatgbfsurnames.csv files/names/languages/frenchsurnames.csv
 
 echo "Cleaning temporal files"
 rm files/tmp/*
