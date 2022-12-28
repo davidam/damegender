8033e20d5267b681f75683feac4b4e3da1761d3b
David Arroyo Menéndez <davidam@gmail.com>
2022-12-28 14:33:34 +0100

    renaming regenerate-intersurnames.sh to sh/regenerate/intersurnames.sh

--- src/damegender/sh/regenerate/intersurnames.sh
+++ src/damegender/sh/regenerate/intersurnames.sh
@@ -20,6 +20,8 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
+cd ../..
+
 echo "Building intersurnames.csv"
 
 touch files/names/names_inter/intersurnames.csv
