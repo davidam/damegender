b3426f0e8976fc100d47ac78455b5e3439820972
David Arroyo Menéndez <davidam@gmail.com>
2023-01-11 16:28:42 +0100

    sh/languages/arabic.sh: adding more countries and males

--- src/damegender/sh/languages/arabic.sh
+++ src/damegender/sh/languages/arabic.sh
@@ -25,10 +25,21 @@ mkdir -p files/tmp
 
 echo "Building arabicfemales.csv"
 echo "Merging arabic countries, building females"
-python3 mergeinterfiles.py --file1="files/names/names_ae/aefemales.csv" --file2="files/names/names_sa/safemales.csv" --output=files/tmp/aesafemales.csv
-python3 mergeinterfiles.py --file1="files/tmp/aesafemales.csv" --file2="files/names/names_sy/syfemales.csv" --output=files/tmp/aesasyfemales.csv
-python3 mergeinterfiles.py --file1="files/tmp/aesasyfemales.csv" --file2="files/names/names_ye/yefemales.csv" --output=files/tmp/aesasyyefemales.csv
-cp files/tmp/aesasyyefemales.csv files/names/languages/arabicfemales.csv
+python3 mergeinterfiles.py --file1="files/names/names_ae/aefemales.csv" --file2="files/names/names_bh/bhfemales.csv" --output=files/tmp/aebhfemales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhfemales.csv" --file2="files/names/names_eg/egfemales.csv" --output=files/tmp/aebhegfemales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegfemales.csv" --file2="files/names/names_sa/safemales.csv" --output=files/tmp/aebhegsafemales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegsafemales.csv" --file2="files/names/names_sy/syfemales.csv" --output=files/tmp/aebhegsasyfemales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegsasyfemales.csv" --file2="files/names/names_ye/yefemales.csv" --output=files/tmp/aebhegsasyyefemales.csv
+cp files/tmp/aebhegsasyyefemales.csv files/names/languages/arabicfemales.csv
+
+echo "Building arabicmales.csv"
+echo "Merging arabic countries, building males"
+python3 mergeinterfiles.py --file1="files/names/names_ae/aemales.csv" --file2="files/names/names_bh/bhmales.csv" --output=files/tmp/aebhmales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhmales.csv" --file2="files/names/names_eg/egmales.csv" --output=files/tmp/aebhegmales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegmales.csv" --file2="files/names/names_sa/samales.csv" --output=files/tmp/aebhegsamales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegsamales.csv" --file2="files/names/names_sy/symales.csv" --output=files/tmp/aebhegsasymales.csv
+python3 mergeinterfiles.py --file1="files/tmp/aebhegsasymales.csv" --file2="files/names/names_ye/yemales.csv" --output=files/tmp/aebhegsasyyemales.csv
+cp files/tmp/aebhegsasyyemales.csv files/names/languages/arabicmales.csv
 
 #echo "Cleaning temporal files"
 #rm files/tmp/*
