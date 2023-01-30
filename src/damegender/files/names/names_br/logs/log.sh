09ef3051278eec1bd6800e955f75024637a37a09
David Arroyo Menendez <davidam@gmail.com>
2023-01-30 14:19:41 +0100

    files/names/names_br/logs/log.sh: creation

---
+++ src/damegender/files/names/names_br/logs/log.sh
@@ -0,0 +1,4 @@
+python3 csv2gender.py files/names/names_br/brfemales.csv --outcsv=files/names/names_br/logs/brfemales.ptguessed.csv --dataset=pt --first_name_position=0
+python3 csv2gender.py files/names/names_br/brmales.csv --outcsv=files/names/names_br/logs/brmales.ptguessed.csv --dataset=pt --first_name_position=0
+python3 csv2gender.py files/names/names_br/brfemales.csv --outcsv=files/names/names_br/logs/brfemales.interguessed.csv --dataset=inter --first_name_position=0
+python3 csv2gender.py files/names/names_br/brmales.csv --outcsv=files/names/names_br/logs/brmales.interguessed.csv --dataset=inter --first_name_position=0
