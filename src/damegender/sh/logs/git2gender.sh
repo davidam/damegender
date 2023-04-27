1bf221f01413e3e058a54bc3ccaed2c871eb733e
David Arroyo Menendez <davidam@gmail.com>
2023-04-27 16:14:00 -0300

    sh/logs/git2gender.sh: adding more git2gender commands emacs lisp and ecma262 (js specification)

--- src/damegender/sh/logs/git2gender.sh
+++ src/damegender/sh/logs/git2gender.sh
@@ -26,3 +26,5 @@ python3 git2gender.py https://github.com/Perl/perl5 --country='inter' --director
 python3 git2gender.py https://github.com/tcltk/tcl --country='inter' --directory="tmp/clonetcl" --show=all --verbose > files/logs/git2gendertcl-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://git.savannah.gnu.org/git/bash.git --country='inter' --directory="tmp/clonebash" --show=all --verbose > files/logs/git2genderbash-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://github.com/r-devel/r-svn --country="inter" --directory="tmp/cloner" --show=all --verbose > files/logs/git2gender-r-devel-$(date "+%Y-%m-%d-%H").txt
+python3 git2gender.py git://git.sv.gnu.org/emacs.git --country="inter" --directory="/tmp/emacs" --show=all --verbose > files/logs/git2gender-emacs-$(date "+%Y-%m-%d-%H").txt
+python3 git2gender.py https://github.com/tc39/ecma262 --country="inter" --directory="/tmp/ecma262" --show=all --verbose > files/logs/ecma262-$(date "+%Y-%m-%d-%H").txt
