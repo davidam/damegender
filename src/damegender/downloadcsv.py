83fc04b679e8bf8960c466ed95bddec9f8298054
David Arroyo Menendez <davidam@gmail.com>
2023-04-29 19:22:36 -0300

    downloadcsv.py: creation

---
+++ src/damegender/downloadcsv.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez (davidam@gmail.com)
+# This file is part of Damegender.
+
+# Author: David Arroyo Menéndez <davidam@gmail.com>
+# Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with DameGender; see the file GPL.txt.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+# DESCRIPTION: This script allows to download a list of names in a
+# csv file from a csv file guessing names, gender and frequency
+# with an api system
+
+from app.dame_gender import Gender
+# from app.dame_namsor import DameNamsor
+# from app.dame_genderize import DameGenderize
+# from app.dame_genderapi import DameGenderApi
+# from app.dame_nameapi import DameNameapi
+from app.dame_brazilapi import DameBrazilApi
+
+import argparse
+import os
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', type=str, required=True,
+                    default="files/names/min.csv", help='input file for names')
+parser.add_argument('--name_position', type=int, required=False,
+                    default=0, help='input file for names')
+parser.add_argument('--api', required=True,
+                    choices=['brazilapi', 'genderapi', 'genderize', 'namsor', 'nameapi'])
+parser.add_argument('--outcsv', type=str, required=False,
+                    default="names.csv", help='output csv file for names')
+args = parser.parse_args()
+
+
+if (args.api == 'brazilapi'):
+    dba = DameBrazilApi()
+    text2 = dba.download_csv(path=args.csv,
+                             name_position=args.name_position,
+                             backup_all=args.outcsv)
+        
