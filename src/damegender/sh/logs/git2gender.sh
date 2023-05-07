d21f2fdc764452d81f0427f8aac43acdf09064fa
David Arroyo Menendez <davidam@gmail.com>
2023-05-07 20:05:55 -0300

    sh/logs/git2gender.sh: adding php and gcc

--- src/damegender/sh/logs/git2gender.sh
+++ src/damegender/sh/logs/git2gender.sh
@@ -25,6 +25,8 @@ python3 git2gender.py https://github.com/python/cpython --country='inter' --dire
 python3 git2gender.py https://github.com/Perl/perl5 --country='inter' --directory="tmp/cloneperl" --show=all --verbose > files/logs/git2genderperl-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://github.com/tcltk/tcl --country='inter' --directory="tmp/clonetcl" --show=all --verbose > files/logs/git2gendertcl-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://git.savannah.gnu.org/git/bash.git --country='inter' --directory="tmp/clonebash" --show=all --verbose > files/logs/git2genderbash-$(date "+%Y-%m-%d-%H").txt
+python3 git2gender.py https://gcc.gnu.org/git/gcc.git --country='inter' --directory="tmp/clonegcc" --show=all --verbose > files/logs/git2gendergcc-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://github.com/r-devel/r-svn --country="inter" --directory="tmp/cloner" --show=all --verbose > files/logs/git2gender-r-devel-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py git://git.sv.gnu.org/emacs.git --country="inter" --directory="/tmp/emacs" --show=all --verbose > files/logs/git2gender-emacs-$(date "+%Y-%m-%d-%H").txt
 python3 git2gender.py https://github.com/tc39/ecma262 --country="inter" --directory="/tmp/ecma262" --show=all --verbose > files/logs/ecma262-$(date "+%Y-%m-%d-%H").txt
+python3 git2gender.py https://github.com/php/php-src.git --directory="/tmp/clonephp" --country="inter" --show=all --verbose > files/logs/git2gender_php-$(date "+%Y-%m-%d-%H").txt
