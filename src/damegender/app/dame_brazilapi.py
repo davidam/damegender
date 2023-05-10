356eedefda623d59e818b9fcb7ede989d0cb39cb
David Arroyo Menendez <davidam@gmail.com>
2023-05-10 15:35:21 -0300

    app/dame_brazilapi.py: fixing troubles if doesn't exist files/tmp folder

--- src/damegender/app/dame_brazilapi.py
+++ src/damegender/app/dame_brazilapi.py
@@ -83,6 +83,9 @@ class DameBrazilApi(Gender):
     
     def download_csv(self, path="files/names/partial.csv", *args, **kwargs):
         # download a csv of people's names from a csv given
+        dir0 = 'files/tmp'
+        if (not os.path.exists(dir0)):
+            os.mkdir('files/tmp/');
         backup_all = kwargs.get('backup_all', 'files/tmp/brall.csv')
         backup_females = kwargs.get('backup_females', 'files/tmp/brfemales.csv')
         backup_males = kwargs.get('backup_males', 'files/tmp/brmales.csv')        
