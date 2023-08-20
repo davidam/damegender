a96cef4c50dc6230dfb7183123eeba1f7b208886
David Arroyo Menéndez <davidam@gmail.com>
2023-08-20 16:38:45 +0200

    sh/tests/toolkit.sh: checking internet connection

--- src/damegender/sh/tests/toolkit.sh
+++ src/damegender/sh/tests/toolkit.sh
@@ -30,85 +30,93 @@ cd ../..
 
 rm -rf /tmp/clonedir
 
-python3 git2gender.py https://github.com/davidam/orgguide-es.git --directory="/tmp/clonedir" > files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt
+wget -q --spider http://google.com
 
-if ! diff files/tests/git2gender1.txt files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
+if [ $? -eq 0 ]; then
+    echo "Online"
+    
+    python3 git2gender.py https://github.com/davidam/orgguide-es.git --directory="/tmp/clonedir" > files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt
+
+    if ! diff files/tests/git2gender1.txt files/tests/git2gender1-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+    then
 	echo -e "git2gender1 test is ${RED}failing${NC}"
-else
+    else
 	echo -e "git2gender1 test is ${GREEN}ok${NC}"
-fi
+    fi
 
-rm -rf /tmp/clonedrupal
+    rm -rf /tmp/clonedrupal
 
-python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --directory="/tmp/clonedrupal" --country=us > files/tests/git2genderdrupal-$(date "+%Y-%m-%d-%H").txt
+    python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --directory="/tmp/clonedrupal" --country=us > files/tests/git2genderdrupal-$(date "+%Y-%m-%d-%H").txt
 
-if ! diff files/tests/git2genderdrupal.txt files/tests/git2genderdrupal-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
+    if ! diff files/tests/git2genderdrupal.txt files/tests/git2genderdrupal-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+    then
 	echo -e "git2genderdrupal test is ${RED}failing${NC}"
-else
+    else
 	echo -e "git2genderdrupal test is ${GREEN}ok${NC}"
-fi
+    fi
 
-python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --directory="/tmp/clonedrupal" --show=all --verbose --country=es > files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt
+    python3 git2gender.py  https://git.drupalcode.org/project/orgmode.git --directory="/tmp/clonedrupal" --show=all --verbose --country=es > files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt
 
-if ! diff files/tests/git2genderverbose.txt files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
+    if ! diff files/tests/git2genderverbose.txt files/tests/git2genderverbose-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+    then
 	echo -e "git2genderverbose test is ${RED}failing${NC}"
-else
+    else
 	echo -e "git2genderverbose test is ${GREEN}ok${NC}"
-fi
+    fi
 
-cd files/mbox
-wget -c http://mail-archives.apache.org/mod_mbox/httpd-announce/201706.mbox
-cd ../..
-python3 mail2gender.py http://mail-archives.apache.org/mod_mbox/httpd-announce/ > files/tests/mail2gender-$(date "+%Y-%m-%d-%H").txt
+    cd files/mbox
+    wget -c http://mail-archives.apache.org/mod_mbox/httpd-announce/201706.mbox
+    cd ../..
+    python3 mail2gender.py http://mail-archives.apache.org/mod_mbox/httpd-announce/ > files/tests/mail2gender-$(date "+%Y-%m-%d-%H").txt
 
-if ! diff files/tests/mail2gender.txt files/tests/mail2gender-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
+    if ! diff files/tests/mail2gender.txt files/tests/mail2gender-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+    then
 	echo -e "mail2gender test is ${RED}failing${NC}"
-else
+    else
 	echo -e "mail2gender test is ${GREEN}ok${NC}"
-fi
+    fi
 
-python3 newspaper2gender.py https://elpais.com/espana/catalunya/2021-11-23/el-presupuesto-de-cataluna-rompe-la-unidad-del-bloque-independentista.html > files/tests/newspaper2gender-$(date "+%Y-%m-%d-%H").txt
+    python3 newspaper2gender.py https://elpais.com/espana/catalunya/2021-11-23/el-presupuesto-de-cataluna-rompe-la-unidad-del-bloque-independentista.html > files/tests/newspaper2gender-$(date "+%Y-%m-%d-%H").txt
 
-if ! diff files/tests/newspaper2gender-$(date "+%Y-%m-%d-%H").txt files/tests/newspaper2gender.txt >/dev/null 2>&1
-then
+    if ! diff files/tests/newspaper2gender-$(date "+%Y-%m-%d-%H").txt files/tests/newspaper2gender.txt >/dev/null 2>&1
+    then
         echo -e  "newspaper2gender test is ${RED}failing${NC}"
         echo -e  "Remember: $ pip3 install newspaper3k"    
-else
+    else
 	echo -e  "newspaper2gender test is ${GREEN}ok${NC}"
-fi
+    fi
 
-python3 newspaper2gender.py https://elpais.com/espana/catalunya/2021-11-23/el-presupuesto-de-cataluna-rompe-la-unidad-del-bloque-independentista.html --total=es > files/tests/newspaper2gender.es-$(date "+%Y-%m-%d-%H").txt
+    python3 newspaper2gender.py https://elpais.com/espana/catalunya/2021-11-23/el-presupuesto-de-cataluna-rompe-la-unidad-del-bloque-independentista.html --total=es > files/tests/newspaper2gender.es-$(date "+%Y-%m-%d-%H").txt
 
-if ! diff files/tests/newspaper2gender.es-$(date "+%Y-%m-%d-%H").txt files/tests/newspaper2gender.es.txt >/dev/null 2>&1
-then
+    if ! diff files/tests/newspaper2gender.es-$(date "+%Y-%m-%d-%H").txt files/tests/newspaper2gender.es.txt >/dev/null 2>&1
+    then
         echo -e  "newspaper2gender spanish test is ${RED}failing${NC}"
         echo -e  "Remember: $ pip3 install newspaper3k"
-else
+    else
 	echo -e  "newspaper2gender spanish test is ${GREEN}ok${NC}"
-fi
+    fi
 
-python3 get-wikidata-names.py female --total=ad
+    python3 get-wikidata-names.py female --total=ad
 
-if [ -f names.csv ]
-then
-       echo -e  "get-wikidata-names.py test is ${GREEN}ok${NC}"
+    if [ -f names.csv ]
+    then
+	echo -e  "get-wikidata-names.py test is ${GREEN}ok${NC}"
+    else
+	echo -e  "get-wikidata-names.py test is ${RED}failing${NC}"
+    fi
+
+    python3 get-wikidata-surnames.py --total=ad
+
+    if [ -f surnames.csv ]
+    then
+	echo -e  "get-wikidata-surnames.py test is ${GREEN}ok${NC}"
+    else
+	echo -e  "get-wikidata-surnames.py test is ${RED}failing${NC}"    
+    fi
+
 else
-       echo -e  "get-wikidata-names.py test is ${RED}failing${NC}"
+    echo "Offline"
 fi
-
-python3 get-wikidata-surnames.py --total=ad
-
-if [ -f surnames.csv ]
-then
-       echo -e  "get-wikidata-surnames.py test is ${GREEN}ok${NC}"
-else
-       echo -e  "get-wikidata-surnames.py test is ${RED}failing${NC}"    
-fi
-
 	
 rm -rf /tmp/clonedir
 rm -rf files/tmp
