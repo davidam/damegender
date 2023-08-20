1983e74976db120affabf9a02e701f21ffcdf0e1
David Arroyo Menéndez <davidam@gmail.com>
2023-08-20 16:34:59 +0200

    sh/tests/apis.sh: checking internet connection

--- src/damegender/sh/tests/apis.sh
+++ src/damegender/sh/tests/apis.sh
@@ -32,175 +32,175 @@ echo "We need enable apis to execute this script, we are trying to do it for you
 
 cp config.enabled.cfg config.cfg
 
+echo "Checking Internet connection"
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+    echo "Online"
+
+    python3 api2gender.py David --api="wikidata" > files/tests/api2genderwikidata-$(date "+%Y-%m-%d-%H").txt
+
+    if ! diff files/tests/api2genderwikidata.txt files/tests/api2genderwikidata-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+    then
+	echo -e "api2genderiwikidata test is ${RED}failing${NC}"
+    else
+	echo -e "api2genderwikidata test is ${GREEN}ok${NC}"
+    fi
+
+    python3 downloadjson.py --api=brazilapi --csv=files/names/min.csv --outjson=files/tmp/brazilapimin.json
+
+    if [ -f files/tmp/brazilapimin.json ]; then
+	echo -e "download brazilapi json files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download brazilapi json files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=brazilapi --csv=files/names/min.csv --outcsv=files/tmp/brazilapimin.csv
+
+    if [ -f files/tmp/brazilapimin.csv ]; then
+	echo -e "download brazilapi csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download brazilapi csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapimin.csv
+
+    if [ -f files/tmp/genderapimin.csv ]; then
+	echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderapi csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminfemales.csv --outformat="females"
+
+    if [ -f files/tmp/genderapiminfemales.csv ]; then
+	echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderapi csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminmales.csv --outformat="males"
+
+    if [ -f files/tmp/genderapiminmales.csv ]; then
+	echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderapi csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminall.csv --outformat="all"
+
+    if [ -f files/tmp/genderapiminall.csv ]; then
+	echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderapi csv files names min is ${RED}failing${NC}"
+    fi
+
+
+    python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminfemales.csv --outformat="females"
+
+    if [ -f files/tmp/genderizeminfemales.csv ]; then
+	echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderize csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminmales.csv --outformat='males'
+
+    if [ -f files/tmp/genderizeminmales.csv ]; then
+	echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderize csv files names min is ${RED}failing${NC}"
+    fi
+
+    python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminall.csv --outformat='all'
+
+    if [ -f files/tmp/genderizeminall.csv ]; then
+	echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
+    else
+	echo -e "download genderize csv files names min is ${RED}failing${NC}"
+    fi
+
+
+    if [ -f files/apikeys/genderizepass.txt ]; then
+
+	python3 api2gender.py David --api="genderize" > files/tests/api2genderDavidgenderize-$(date "+%Y-%m-%d-%H").txt
+
+	if ! diff files/tests/api2genderDavidgenderize.txt files/tests/api2genderDavidgenderize-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+	then
+	    echo -e "api2genderDavidgenderize test is ${RED}failing${NC}"
+	else
+	    echo -e "api2genderDavidgenderize test is ${GREEN}ok${NC}"
+	fi
+
+	python3 downloadjson.py --api=genderize --csv=files/names/min.csv
+
+	if [ -f files/names/genderizefiles_names_min.csv.json ]; then
+	    echo -e "download genderize files names min is ${GREEN}ok${NC}"
+	else
+	    echo -e "download genderize files names min is ${RED}failing${NC}"
+	fi
+    else
+	echo -e "Doesn't exist files/apikeys/genderizepass.txt. You must introduce the api key in this file"
+    fi
+
+    if [ -f files/apikeys/namsorpass.txt ]; then
+	python3 api2gender.py Leticia --surname="Martin" --api="namsor" > files/tests/api2genderLeticianamsor-$(date "+%Y-%m-%d-%H").txt
+
+	if ! diff files/tests/api2genderLeticianamsor.txt files/tests/api2genderLeticianamsor-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+	then
+	    echo -e "api2genderLeticianamsor test is ${RED}failing${NC}"
+	else
+	    echo -e "api2genderLeticianamsor test is ${GREEN}ok${NC}"
+	fi
+
+	python3 downloadjson.py --api=namsor --csv=files/names/min.csv
+
+	if [ -f files/names/namsorfiles_names_min.csv.json ]; then
+	    echo -e "download namsor files names min is ${GREEN}ok${NC}"
+	else
+	    echo -e "download namsor files names min is ${RED}failing${NC}"
+	fi
+    else
+	echo -e "Doesn't exist files/apikeys/namsorpass.txt. You must introduce the api key in this file"
+
+    fi
+
+    if [ -f files/apikeys/genderapipass.txt ]; then
+
+	python3 downloadjson.py --api=genderapi --csv=files/names/min.csv
+
+	if [ -f files/names/genderapifiles_names_min.csv.json ]; then
+	    echo -e "download genderapi files names min is ${GREEN}ok${NC}"
+	else
+	    echo -e "download genderapi files names min is ${RED}failing${NC}"
+	fi
+
+	python3 downloadjson.py --api=genderapi --csv=files/names/names_tests/gender_JMLA.min.dta.csv --name_position=1
+
+	if [ -f files/names/genderapifiles_names_min.csv.json ]; then
+	    echo -e "download genderapi files names min is ${GREEN}ok${NC}"
+	else
+	    echo -e "download genderapi files names min is ${RED}failing${NC}"
+	fi
+
+	
+	python3 api2gender.py Inés --api="genderapi" > files/tests/api2genderInésgenderapi-$(date "+%Y-%m-%d-%H").txt
+
+	if ! diff files/tests/api2genderInésgenderapi.txt files/tests/api2genderInésgenderapi-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
+	then
+	    echo -e "api2genderInésgenderapi test is ${RED}failing${NC}"
+	else
+	    echo -e "api2genderInésgenderapi test is ${GREEN}ok${NC}"
+	fi
+
+    else
+	echo "Doesn't exist files/apikeys/genderapipass.txt. You must introduce the api key in this file"
+
+    fi
+
     
-python3 api2gender.py David --api="wikidata" > files/tests/api2genderwikidata-$(date "+%Y-%m-%d-%H").txt
-
-if ! diff files/tests/api2genderwikidata.txt files/tests/api2genderwikidata-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-then
-    echo -e "api2genderiwikidata test is ${RED}failing${NC}"
 else
-    echo -e "api2genderwikidata test is ${GREEN}ok${NC}"
-fi
-
-python3 downloadjson.py --api=brazilapi --csv=files/names/min.csv --outjson=files/tmp/brazilapimin.json
-
-if [ -f files/tmp/brazilapimin.json ]; then
-    echo -e "download brazilapi json files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download brazilapi json files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=brazilapi --csv=files/names/min.csv --outcsv=files/tmp/brazilapimin.csv
-
-if [ -f files/tmp/brazilapimin.csv ]; then
-    echo -e "download brazilapi csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download brazilapi csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapimin.csv
-
-if [ -f files/tmp/genderapimin.csv ]; then
-    echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderapi csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminfemales.csv --outformat="females"
-
-if [ -f files/tmp/genderapiminfemales.csv ]; then
-    echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderapi csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminmales.csv --outformat="males"
-
-if [ -f files/tmp/genderapiminmales.csv ]; then
-    echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderapi csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderapi --csv=files/names/min.csv --outcsv=files/tmp/genderapiminall.csv --outformat="all"
-
-if [ -f files/tmp/genderapiminall.csv ]; then
-    echo -e "download genderapi csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderapi csv files names min is ${RED}failing${NC}"
-fi
-
-
-python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminfemales.csv --outformat="females"
-
-if [ -f files/tmp/genderizeminfemales.csv ]; then
-    echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderize csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminmales.csv --outformat='males'
-
-if [ -f files/tmp/genderizeminmales.csv ]; then
-    echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderize csv files names min is ${RED}failing${NC}"
-fi
-
-python3 downloadcsv.py --api=genderize --csv=files/names/min.csv --outcsv=files/tmp/genderizeminall.csv --outformat='all'
-
-if [ -f files/tmp/genderizeminall.csv ]; then
-    echo -e "download genderize csv files names min is ${GREEN}ok${NC}"
-else
-    echo -e "download genderize csv files names min is ${RED}failing${NC}"
-fi
-
-
-# python3 api2gender.py David --api="wikipedia" > files/tests/api2genderwikipedia-$(date "+%Y-%m-%d-%H").txt
-
-# if ! diff files/tests/api2genderwikipedia.txt files/tests/api2genderwikipedia-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-# then
-#    echo -e "api2genderiwikipedia test is ${RED}failing${NC}"
-# else
-#    echo -e "api2genderwikipedia test is ${GREEN}ok${NC}"
-# fi
-
-
-if [ -f files/apikeys/genderizepass.txt ]; then
-
-    python3 api2gender.py David --api="genderize" > files/tests/api2genderDavidgenderize-$(date "+%Y-%m-%d-%H").txt
-
-    if ! diff files/tests/api2genderDavidgenderize.txt files/tests/api2genderDavidgenderize-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-    then
-	echo -e "api2genderDavidgenderize test is ${RED}failing${NC}"
-    else
-	echo -e "api2genderDavidgenderize test is ${GREEN}ok${NC}"
-    fi
-
-    python3 downloadjson.py --api=genderize --csv=files/names/min.csv
-
-    if [ -f files/names/genderizefiles_names_min.csv.json ]; then
-	echo -e "download genderize files names min is ${GREEN}ok${NC}"
-    else
-	echo -e "download genderize files names min is ${RED}failing${NC}"
-    fi
-else
-    echo -e "Doesn't exist files/apikeys/genderizepass.txt. You must introduce the api key in this file"
-fi
-
-if [ -f files/apikeys/namsorpass.txt ]; then
-    python3 api2gender.py Leticia --surname="Martin" --api="namsor" > files/tests/api2genderLeticianamsor-$(date "+%Y-%m-%d-%H").txt
-
-    if ! diff files/tests/api2genderLeticianamsor.txt files/tests/api2genderLeticianamsor-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-    then
-	echo -e "api2genderLeticianamsor test is ${RED}failing${NC}"
-    else
-	echo -e "api2genderLeticianamsor test is ${GREEN}ok${NC}"
-    fi
-
-    python3 downloadjson.py --api=namsor --csv=files/names/min.csv
-
-    if [ -f files/names/namsorfiles_names_min.csv.json ]; then
-	echo -e "download namsor files names min is ${GREEN}ok${NC}"
-    else
-	echo -e "download namsor files names min is ${RED}failing${NC}"
-    fi
-else
-    echo -e "Doesn't exist files/apikeys/namsorpass.txt. You must introduce the api key in this file"
-
-fi
-
-if [ -f files/apikeys/genderapipass.txt ]; then
-
-    python3 downloadjson.py --api=genderapi --csv=files/names/min.csv
-
-    if [ -f files/names/genderapifiles_names_min.csv.json ]; then
-	echo -e "download genderapi files names min is ${GREEN}ok${NC}"
-    else
-	echo -e "download genderapi files names min is ${RED}failing${NC}"
-    fi
-
-    python3 downloadjson.py --api=genderapi --csv=files/names/names_tests/gender_JMLA.min.dta.csv --name_position=1
-
-    if [ -f files/names/genderapifiles_names_min.csv.json ]; then
-	echo -e "download genderapi files names min is ${GREEN}ok${NC}"
-    else
-	echo -e "download genderapi files names min is ${RED}failing${NC}"
-    fi
-
-    
-    python3 api2gender.py Inés --api="genderapi" > files/tests/api2genderInésgenderapi-$(date "+%Y-%m-%d-%H").txt
-
-    if ! diff files/tests/api2genderInésgenderapi.txt files/tests/api2genderInésgenderapi-$(date "+%Y-%m-%d-%H").txt >/dev/null 2>&1
-    then
-	echo -e "api2genderInésgenderapi test is ${RED}failing${NC}"
-    else
-	echo -e "api2genderInésgenderapi test is ${GREEN}ok${NC}"
-    fi
-
-else
-    echo "Doesn't exist files/apikeys/genderapipass.txt. You must introduce the api key in this file"
-
+    echo "Offline"
 fi
 
 echo "cleaning temporary files"
