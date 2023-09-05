bd634678c6e1f2847553e8f86072e33229cb96af
David Arroyo Menéndez <davidam@gmail.com>
2023-09-05 12:05:17 +0200

    sh/languages/spanish.sh: adding more information

--- src/damegender/sh/languages/spanish.sh
+++ src/damegender/sh/languages/spanish.sh
@@ -23,30 +23,33 @@
 cd ../../
 mkdir -p files/tmp
 
+echo "Countries where spanish is the majoritary language:"
+echo "Chile, Colombia, Costa Rica, Cuba, Dominican Republic, Ecuador, El Salvador, Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Puerto Rico, Uruguay, and Venezuela" 
+
 echo "Building spanishfemales.csv"
-echo "Merging Argentina and Spanish"
+echo "Merging Argentina (statistics office) and Spanish (statistic office)"
 python3 mergeinterfiles.py --file1="files/names/names_ar/arfemales.csv" --file2="files/names/names_es/esfemales.csv" --output=files/tmp/aresfemales.csv
-echo "Merging Uruguay"
+echo "Merging Uruguay (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresfemales.csv" --file2="files/names/names_uy/uyfemales.csv" --output=files/tmp/aresuyfemales.csv
-echo "Merging Mexico"
+echo "Merging Mexico (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuyfemales.csv" --file2="files/names/names_mx/mxfemales.csv" --output=files/tmp/aresuymxfemales.csv
-echo "Merging Venezuela"
+echo "Merging Venezuela (wikidata)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuymxfemales.csv" --file2="files/names/names_ve/vefemales.csv" --output=files/tmp/aresuymxvefemales.csv
-echo "Merging Chile"
+echo "Merging Chile (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuymxvefemales.csv" --file2="files/names/names_cl/clfemales.csv" --output=files/tmp/aresuymxveclfemales.csv
 cp files/tmp/aresuymxveclfemales.csv files/names/languages/spanishfemales.csv
 
 
 echo "Building spanishmales.csv"
-echo "Merging Argentina and Spanish"
+echo "Merging Argentina (statistics office) and Spanish (statistic office)"
 python3 mergeinterfiles.py --file1="files/names/names_ar/armales.csv" --file2="files/names/names_es/esmales.csv" --output=files/tmp/aresmales.csv
-echo "Merging Uruguay"
+echo "Merging Uruguay (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresmales.csv" --file2="files/names/names_uy/uymales.csv" --output=files/tmp/aresuymales.csv
-echo "Merging Mexico"
+echo "Merging Mexico (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuymales.csv" --file2="files/names/names_mx/mxmales.csv" --output=files/tmp/aresuymxmales.csv
-echo "Merging Venezuela"
+echo "Merging Venezuela (wikidata)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuymxmales.csv" --file2="files/names/names_ve/vemales.csv" --output=files/tmp/aresuymxvemales.csv
-echo "Merging Chile"
+echo "Merging Chile (statistics office)"
 python3 mergeinterfiles.py --file1="files/tmp/aresuymxvefemales.csv" --file2="files/names/names_cl/clmales.csv" --output=files/tmp/aresuymxveclfemales.csv
 
 cp files/tmp/aresuymxveclmales.csv files/names/languages/spanishmales.csv
