df70c81e57ab69999785943c3d596c59cfdfee5f
David Arroyo Menéndez <davidam@gmail.com>
2023-09-30 21:22:57 +0200

    app/dame_nameapi.py: adding try/except to requests

--- src/damegender/app/dame_nameapi.py
+++ src/damegender/app/dame_nameapi.py
@@ -24,13 +24,18 @@
 
 
 import csv
-import requests
 import json
-import requests
 import configparser
 import os
 from app.dame_gender import Gender
 from app.dame_utils import DameUtils
+try:
+    import requests
+except:
+    print("module 'requests' is not installed")
+    print("try:")
+    print("$ pip3 install 'requests'")
+    exit()    
 
 
 class DameNameapi(Gender):
